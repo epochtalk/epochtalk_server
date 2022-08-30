4da8a4e1b8ceeea69000f1a2df4c42a79f7edda7
Anthony Kinsey <anthony@slickage.com>
2022-08-29 14:56:19 -1000

    feat(deps): add iteraptor dependency for seed

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule EpochtalkServer.MixProject do
       {:phoenix, "~> 1.6.11"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
+      {:iteraptor, git: "https://github.com/epochtalk/elixir-iteraptor.git", tag: "1.13.1"},
       {:postgrex, ">= 0.0.0"},
       {:swoosh, "~> 1.3"},
       {:telemetry_metrics, "~> 0.6"},

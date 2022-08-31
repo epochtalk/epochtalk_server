6fcd4e8cca1b1033fe20679bd55df6ffdf9bbefa
unenglishable <unenglishable@gmail.com>
2022-08-30 14:52:38 -1000

    feat(guardian): add guardian app configurations
    handle resource, claims, subject
    
    copied from epoch

---
+++ lib/epochtalk_server/auth/guardian.ex
@@ -0,0 +1,51 @@
+defmodule EpochtalkServer.Auth.Guardian do
+  use Guardian, otp_app: :epochtalk_server
+
+  def subject_for_token(%{user_id: user_id}, _claims) do
+    # You can use any value for the subject of your token but
+    # it should be useful in retrieving the resource later, see
+    # how it being used on `resource_from_claims/1` function.
+    # A unique `id` is a good subject, a non-unique email address
+    # is a poor subject.
+    sub = to_string(user_id)
+    {:ok, sub}
+  end
+  def subject_for_token(_, _) do
+    {:error, :reason_for_error}
+  end
+
+  def resource_from_claims(%{"sub" => user_id}) do
+    # Here we'll look up our resource from the claims, the subject can be
+    # found in the `"sub"` key. In above `subject_for_token/2` we returned
+    # the resource id so here we'll rely on that to look it up.
+    resource = EpochtalkServer.User.by_id(user_id)
+    {:ok,  resource}
+  end
+  def resource_from_claims(_claims) do
+    {:error, :reason_for_error}
+  end
+
+  def after_encode_and_sign(resource, claims, token, _options) do
+    with {:ok, _} <- Guardian.DB.after_encode_and_sign(resource, claims["typ"], claims, token) do
+      {:ok, token}
+    end
+  end
+
+  def on_verify(claims, token, _options) do
+    with {:ok, _} <- Guardian.DB.on_verify(claims, token) do
+      {:ok, claims}
+    end
+  end
+
+  def on_refresh({old_token, old_claims}, {new_token, new_claims}, _options) do
+    with {:ok, _, _} <- Guardian.DB.on_refresh({old_token, old_claims}, {new_token, new_claims}) do
+      {:ok, {old_token, old_claims}, {new_token, new_claims}}
+    end
+  end
+
+  def on_revoke(claims, token, _options) do
+    with {:ok, _} <- Guardian.DB.on_revoke(claims, token) do
+      {:ok, claims}
+    end
+  end
+end

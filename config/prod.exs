f1c01568b299ac3e76fc6909e875a866b78bae4f
unenglishable <unenglishable@gmail.com>
2022-09-01 13:54:54 -1000

    chore(prod config): include prod.secret.exs if available

--- config/prod.exs
+++ config/prod.exs
@@ -47,3 +47,7 @@ config :logger, level: :info
 #       force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
+
+if File.exists?("config/prod.secret.exs") do
+  import_config "prod.secret.exs"
+end

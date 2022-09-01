922bc4144b23fb3e68488459173b2663c9f8186a
unenglishable <unenglishable@gmail.com>
2022-09-01 13:35:14 -1000

    feat(dev config): set debug_errors: false

--- config/dev.exs
+++ config/dev.exs
@@ -22,7 +22,7 @@ config :epochtalk_server, EpochtalkServerWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4000],
   check_origin: false,
   code_reloader: true,
-  debug_errors: true,
+  debug_errors: false,
   secret_key_base: "9ORa6oGSN+xlXNedSn0gIKVc/6//naQqSiZsRJ8vNbcvHpPOTPMLgcn134WIH3Pd",
   watchers: []
 

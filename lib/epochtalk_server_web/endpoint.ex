6be4b059f46c62878fabdb4aac8991e0191e0b96
unenglishable <unenglishable@gmail.com>
2022-09-01 14:07:44 -1000

    refactor(endpoint): remove session options
    not being used because this is a json api

--- lib/epochtalk_server_web/endpoint.ex
+++ lib/epochtalk_server_web/endpoint.ex
@@ -1,15 +1,6 @@
 defmodule EpochtalkServerWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :epochtalk_server
 
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  @session_options [
-    store: :cookie,
-    key: "_epochtalk_server_key",
-    signing_salt: "0GPyuH5f"
-  ]
-
   # socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
@@ -39,6 +30,5 @@ defmodule EpochtalkServerWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
-  plug Plug.Session, @session_options
   plug EpochtalkServerWeb.Router
 end

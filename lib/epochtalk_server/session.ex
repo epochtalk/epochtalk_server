1ad6cfed9b204c4f58bdb30adbbb477605397506
unenglishable <unenglishable@gmail.com>
2022-09-06 11:26:02 -1000

    feat(session): stub session.ex
    contains methods from server/plugins/session from old js project

---
+++ lib/epochtalk_server/session.ex
@@ -0,0 +1,17 @@
+defmodule EpochtalkServer.Session do
+  def save do
+
+  end
+  def update_roles do
+
+  end
+  def update_moderating do
+
+  end
+  def update_user_info do
+
+  end
+  def update_ban_info do
+
+  end
+end

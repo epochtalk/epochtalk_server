a93ea2adf3f627166927885be4c39e4a2e7f7930
Anthony Kinsey <anthony@slickage.com>
2022-09-06 16:15:06 -1000

    refactor(user-query): wip implement usersByUsername query

--- lib/epochtalk_server/models/user.ex
+++ lib/epochtalk_server/models/user.ex
@@ -4,6 +4,8 @@ defmodule EpochtalkServer.Models.User do
   import Ecto.Query
   alias EpochtalkServer.Repo
   alias EpochtalkServer.Models.User
+  alias EpochtalkServer.Models.Profile
+  alias EpochtalkServer.Models.Preference
 
   schema "users" do
     field :email, :string
@@ -36,12 +38,57 @@ defmodule EpochtalkServer.Models.User do
   def with_username_exists?(username), do: Repo.exists?(from u in User, where: u.username == ^username)
   def with_email_exists?(email), do: Repo.exists?(from u in User, where: u.email == ^email)
   def by_id(id) when is_integer(id), do: Repo.get_by(User, id: id)
+  def by_username(username) when is_binary(username) do
+    query = from u in User,
+    left_join: p in Profile,
+      where: u.id == p.user_id,
+    left_join: pr in Preference,
+      where: u.id == pr.user_id,
+    select: {u.id,
+      u.username,
+      u.email,
+      u.passhash,
+      u.confirmation_token,
+      u.reset_token,
+      u.reset_expiration,
+      u.deleted,
+      u.malicious_score,
+      u.created_at,
+      u.updated_at,
+      u.imported_at,
+      p.avatar,
+      p.position,
+      p.signature,
+      p.raw_signature,
+      p.fields,
+      p.post_count,
+      p.last_active,
+      pr.posts_per_page,
+      pr.threads_per_page,
+      pr.collapsed_categories,
+      pr.ignored_boards,
+      fragment(
+       """
+       CASE WHEN EXISTS (
+         SELECT user_id
+         FROM roles_users
+         WHERE role_id = (SELECT id FROM roles WHERE lookup = \'banned\') and user_id = u.id
+       )
+       THEN (
+         SELECT expiration
+         FROM users.bans
+         WHERE user_id = u.id
+       )
+       ELSE NULL END AS ban_expiration,
+       """)}
+      Repo.one(query)
+  end
   def by_username_and_password(username, password)
       when is_binary(username) and is_binary(password) do
     user = Repo.get_by(User, username: username)
     if User.valid_password?(user, password), do: user
   end
-  def valid_password?(%EpochtalkServer.Model.User{passhash: hashed_password}, password)
+  def valid_password?(%User{passhash: hashed_password}, password)
       when is_binary(hashed_password) and byte_size(password) > 0 do
     Argon2.verify_pass(password, hashed_password)
   end
@@ -55,7 +102,7 @@ defmodule EpochtalkServer.Models.User do
     |> validate_required([:email])
     |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
     |> validate_length(:email, max: 160)
-    |> unsafe_validate_unique(:email, EpochtalkServer.Repo)
+    |> unsafe_validate_unique(:email, Repo)
     |> unique_constraint(:email)
   end
   defp validate_password(changeset) do

65dbae181895e6f5798a5efffa9fae03dc51997d
unenglishable <unenglishable@gmail.com>
2022-09-06 15:09:13 -1000

    feat(session.save): set default role if necessary

--- lib/epochtalk_server/session.ex
+++ lib/epochtalk_server/session.ex
@@ -1,6 +1,17 @@
 defmodule EpochtalkServer.Session do
-  def save do
+  def save(db_user) do
+    # replace db_user role with role lookups
+    # |> Map.put(:role, Enum.map(db_user.roles, &(&1.lookup))
+    # actually, just assume role lookups are there
 
+    # set default role if necessary
+    roles = db_user.roles
+    |> List.length
+    |> case do
+      0 -> ['user']
+      _ -> db_user.roles
+    end
+    db_user = Map.put(db_user, :roles, roles)
   end
   def update_roles do
 

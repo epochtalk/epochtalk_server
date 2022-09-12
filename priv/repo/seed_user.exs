fd0683961fe971c06e8092a63959a3043ce1c4a3
unenglishable <unenglishable@gmail.com>
2022-09-12 12:34:48 -1000

    refactor(seed-user): use code in User/RoleUser to seed user with role
    removed old lib/epochtalk_server/seed/user.ex since functionality is
    covered in the models now
    
    fix function overloading on System.argv by checking with case

--- priv/repo/seed_user.exs
+++ priv/repo/seed_user.exs
@@ -1,3 +1,12 @@
-alias EpochtalkServer.Seed.User
+alias EpochtalkServer.Models.User
 System.argv()
-|> User.process_args
+|> case do
+  [username, email, password, "admin" = _admin] ->
+    admin = true
+    User.seed_user(username, email, password, admin)
+  [username, email, password] ->
+    User.seed_user(username, email, password)
+end
+|> case do
+  {:ok, _} -> IO.puts("Successfully seeded user")
+  {:error, _} -> IO.puts("Error seeding user")

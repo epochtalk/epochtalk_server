723b5b112282f23e15dccabba2b19799483a7f54
Anthony Kinsey <anthony@slickage.com>
2022-09-12 14:59:49 -1000

    feat(category-model): implement update_for_board_mapping

--- lib/epochtalk_server/models/category.ex
+++ lib/epochtalk_server/models/category.ex
@@ -18,10 +18,22 @@ defmodule EpochtalkServer.Models.Category do
     many_to_many :boards, Board, join_through: BoardMapping
   end
 
-  def changeset(board, attrs) do
-    board
+  def changeset(category, attrs) do
+    category
     |> cast(attrs, [:id, :name, :view_order, :viewable_by, :postable_by, :created_at, :imported_at, :updated_at, :meta])
     |> unique_constraint(:id, name: :categories_pkey)
   end
-  def insert(%Category{} = category), do: Repo.insert(category)
+  def insert(%Category{} = cat), do: Repo.insert(cat)
+
+  def changeset_for_board_mapping(category, attrs) do
+    category
+    |> cast(attrs, [:id, :name, :view_order, :viewable_by, :postable_by])
+    |> unique_constraint(:id, name: :categories_pkey)
+  end
+
+  def update_for_board_mapping(%{ id: id } = category_map) do
+    %Category{ id: id }
+    |> changeset_for_board_mapping(category_map)
+    |> Repo.update
+  end
 end

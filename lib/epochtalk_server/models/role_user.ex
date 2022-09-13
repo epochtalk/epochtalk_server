bf93807ac0d45ec28796bd6d12c4265da595e48d
Anthony Kinsey <anthony@slickage.com>
2022-09-13 09:53:12 -1000

    feat(roles-users): implement delete for roles users

--- lib/epochtalk_server/models/role_user.ex
+++ lib/epochtalk_server/models/role_user.ex
@@ -1,8 +1,11 @@
 defmodule EpochtalkServer.Models.RoleUser do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query, only: [from: 2]
+  alias EpochtalkServer.Repo
   alias EpochtalkServer.Models.User
   alias EpochtalkServer.Models.Role
+  alias EpochtalkServer.Models.RoleUser
 
   schema "roles_users" do
     belongs_to :user, User
@@ -13,5 +16,11 @@ defmodule EpochtalkServer.Models.RoleUser do
     permission
     |> cast(attrs, [:user_id, :role_id])
     |> validate_required([:user_id, :role_id])
+  end
+
+  def delete(role_id, user_id) do
+    query = from ru in RoleUser,
+      where: ru.role_id == ^role_id and ru.user_id == ^user_id
+    Repo.delete_all(query)
   end
 end

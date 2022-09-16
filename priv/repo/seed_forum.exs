5c68385d9954e81466e48b27179426ed620e70fb
unenglishable <unenglishable@gmail.com>
2022-09-16 10:14:37 -1000

    feat(seed-forum): add board seed, refactor

--- priv/repo/seed_forum.exs
+++ priv/repo/seed_forum.exs
@@ -1,9 +1,28 @@
 alias EpochtalkServer.Models.Category
 category_name = "General"
+category = %Category{
+  name: category_name
+}
 
-Category.insert(%Category{name: category_name})
+board_name = "General Discussion"
+board_description = "Every forum's got one; talk about anything here"
+board = %Board{
+  name: board_name,
+  description: board_description
+}
+
+seeded_category = Category.insert(category_seed)
 |> case do
-  {:ok, category} ->
-    IO.inspect(category)
-  _ -> IO.puts("Seed failed, unable to create Category with name #{category_name}")
+  {:ok, c} -> c
+  _ ->
+    IO.puts("Seed failed, unable to create Category with name #{category_name}")
+    Process.exit(self, :normal)
+end
+
+seeded_board = Board.insert(board_seed)
+|> case do
+  {:ok, b} -> b
+  _ ->
+    IO.puts("Seed failed, unable to create Board #{board_name}, #{board_description}")
+    Process.exit(self, :normal)
 end

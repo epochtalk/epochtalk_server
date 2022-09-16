65ceb2e8989ae58101a879750c46a6d742ce202a
Anthony Kinsey <anthony@slickage.com>
2022-09-16 12:34:12 -1000

    refactor(role-user): rename delete->delete_user_role

--- lib/epochtalk_server/models/role_user.ex
+++ lib/epochtalk_server/models/role_user.ex
@@ -26,7 +26,7 @@ defmodule EpochtalkServer.Models.RoleUser do
     |> Repo.insert
   end
 
-  def delete(role_id, user_id) do
+  def delete_user_role(role_id, user_id) do
     query = from ru in RoleUser,
       where: ru.role_id == ^role_id and ru.user_id == ^user_id
     Repo.delete_all(query)

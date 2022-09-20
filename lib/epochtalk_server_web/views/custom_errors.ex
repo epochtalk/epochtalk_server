1149839343e5af9aa6f1aacda1aadd590d25d253
unenglishable <unenglishable@gmail.com>
2022-09-19 14:21:05 -1000

    refactor(errors): add auth prefix to all auth-related errors

--- lib/epochtalk_server_web/views/custom_errors.ex
+++ lib/epochtalk_server_web/views/custom_errors.ex
@@ -1,14 +1,17 @@
 defmodule EpochtalkServerWeb.CustomErrors do
+  @auth_prefix "Auth:"
+
+  # Auth errors
   defmodule InvalidCredentials do
     @moduledoc """
     Exception raised when user and password are not correct
     """
-    defexception plug_status: 400, message: "Invalid credentials", conn: nil, router: nil
+    defexception plug_status: 400, message: "#{auth_prefix} Invalid credentials", conn: nil, router: nil
   end
   defmodule NotLoggedIn do
     @moduledoc """
     Exception raised when user is not logged in
     """
-    defexception plug_status: 400, message: "Not logged in", conn: nil, router: nil
+    defexception plug_status: 400, message: "#{auth_prefix} Not logged in", conn: nil, router: nil
   end
 end

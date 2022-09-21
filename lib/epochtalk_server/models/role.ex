7ef9f79a1eed0287735e71b80d3814039381b1f6
Anthony Kinsey <anthony@slickage.com>
2022-09-20 20:02:14 -1000

    feat(role): implement get_masked_permissions to xor all user's role's permissions

--- lib/epochtalk_server/models/role.ex
+++ lib/epochtalk_server/models/role.ex
@@ -59,4 +59,34 @@ defmodule EpochtalkServer.Models.Role do
   defp reduce_ban_role([]), do: nil
   defp reduce_ban_role([role | _]) when role.lookup === "banned", do: role
   defp reduce_ban_role([_ | roles]), do: reduce_ban_role(roles)
+
+  def get_masked_permissions(roles) when is_list(roles), do: Enum.reduce(roles, %{}, &get_masked_permissions(&2, &1))
+  def get_masked_permissions(target, source) do
+    merge_keys = [:highlight_color, :permissions, :priority, :priority_restrictions]
+    filtered_source_keys = Map.keys(source) |> Enum.filter(&Enum.member?(merge_keys, &1))
+    target_is_lesser_role = !Map.get(target, :priority) or target.priority > source.priority
+    Enum.reduce(filtered_source_keys, %{}, fn key, acc ->
+      case key do
+        :priority_restrictions -> # merge priority restrictions
+          source_pr = source.priority_restrictions
+          if target_is_lesser_role and !!source_pr and length(source_pr),
+            do: Map.put(acc, :priority_restrictions, source_pr),
+            else: Map.put(acc, :priority_restrictions, Map.get(target, :priority_restrictions))
+        :permissions -> # merge permissions
+          target_permissions = if p = Map.get(target, key), do: p, else: %{}
+          Map.put(acc, key, deep_merge(target_permissions, Map.get(source, key)))
+        key when key in [:priority, :highlight_color] -> # merge priority/highlight_color
+          if target_is_lesser_role, do: Map.put(acc, key, Map.get(source, key)), else: Map.put(acc, key, Map.get(target, key))
+      end
+    end)
+  end
+
+  def deep_merge(left, right), do: Map.merge(left, right, &deep_resolve/3)
+  # Key exists in both maps, and both values are maps as well.
+  # These can be merged recursively.
+  defp deep_resolve(_key, left = %{}, right = %{}), do: deep_merge(left, right)
+  # Key exists in both maps, but at least one of the values is
+  # NOT a map. We fall back to standard merge behavior, preferring
+  # the value on the right.
+  defp deep_resolve(_key, _left, right), do: right
 end

fb4bcd03445df6a0f732afb8efe1c78856e81987
Anthony Kinsey <anthony@slickage.com>
2022-09-29 15:10:43 -1000

    refactor(guardian-error-handler): modify guardian error handler to user ErrorHelpers.render_json_error

--- lib/epochtalk_server_web/views/guardian_error_handler.ex
+++ lib/epochtalk_server_web/views/guardian_error_handler.ex
@@ -1,16 +1,15 @@
 defmodule EpochtalkServerWeb.GuardianErrorHandler do
-  use Phoenix.Controller
-  alias EpochtalkServerWeb.ErrorView
+  alias EpochtalkServerWeb.ErrorHelpers
 
-  # This is used to convert errors coming out of the guardian pipelines into a 401
-  # renders error tuple from guardian pipeline into a json error response
+  @doc """
+  This is used to convert errors coming out of the guardian pipelines into a 401
+  renders error tuple from guardian pipeline into a json error response
+  """
   @behaviour Guardian.Plug.ErrorHandler
   @impl Guardian.Plug.ErrorHandler
   def auth_error(conn, {type, _reason}, _opts) do
+    # convert type atom into human readable string (ex :invalid_token -> Invalid token)
     message = to_string(type) |> String.replace("_", " ") |> String.capitalize
-    conn
-    |> put_status(401)
-    |> put_view(ErrorView)
-    |> render("401.json", message: message)
+    ErrorHelpers.render_json_error(conn, 401, message)
   end
 end

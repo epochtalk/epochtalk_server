1e41d65d290f6fd6d525713d508aa0bc0ce4f956
Anthony Kinsey <anthony@slickage.com>
2022-10-03 12:13:20 -1000

    refactor(board-moderator): remove unused code, now that we are doing things the Ecto way.

--- lib/epochtalk_server/models/board_moderator.ex
+++ lib/epochtalk_server/models/board_moderator.ex
@@ -20,19 +20,4 @@ defmodule EpochtalkServer.Models.BoardModerator do
     |> cast(attrs, [:user_id, :board_id])
     |> validate_required([:user_id, :board_id])
   end
-
-  ## === Database Functions ===
-
-  defp get_boards_by_user_id(user_id) when is_integer(user_id) do
-    from(bm in BoardModerator, where: bm.user_id == ^user_id)
-    |> Repo.all()
-    |> Enum.map(&(&1.board_id))
-  end
-
-  def get_boards(%{ id: user_id } = user) do
-    moderating = get_boards_by_user_id(user_id)
-    if length(moderating) > 1,
-      do: {:ok, Map.put(user, :moderating, moderating)},
-      else: {:ok, user}
-  end
 end

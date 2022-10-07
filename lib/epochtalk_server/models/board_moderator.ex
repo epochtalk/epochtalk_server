cba4507c5d62eccb6a68e8e4121cf8c6a349cbcb
Anthony Kinsey <anthony@slickage.com>
2022-10-06 16:35:19 -1000

    docs(board-moderator): add docs for board moderator model

--- lib/epochtalk_server/models/board_moderator.ex
+++ lib/epochtalk_server/models/board_moderator.ex
@@ -3,6 +3,9 @@ defmodule EpochtalkServer.Models.BoardModerator do
   import Ecto.Changeset
   alias EpochtalkServer.Models.User
   alias EpochtalkServer.Models.Board
+  @moduledoc """
+  `BoardModerator` model, for performing actions relating to `Board` moderators
+  """
 
   @primary_key false
   schema "board_moderators" do
@@ -12,8 +15,15 @@ defmodule EpochtalkServer.Models.BoardModerator do
 
   ## === Changesets Functions ===
 
-  def changeset(permission, attrs \\ %{}) do
-    permission
+  @doc """
+  Create generic changeset for `BoardModerator` model
+  """
+  @spec changeset(
+    board_moderator :: %EpochtalkServer.Models.BoardModerator{},
+    attrs :: %{} | nil
+  ) :: %EpochtalkServer.Models.Board{}
+  def changeset(board_moderator, attrs \\ %{}) do
+    board_moderator
     |> cast(attrs, [:user_id, :board_id])
     |> validate_required([:user_id, :board_id])
   end

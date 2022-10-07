a934fd68f1a4e81d208ff727f1a4db0afe6ea7d8
Anthony Kinsey <anthony@slickage.com>
2022-10-06 16:36:52 -1000

    docs(permission): add docs for permission model

--- lib/epochtalk_server/models/permission.ex
+++ lib/epochtalk_server/models/permission.ex
@@ -3,6 +3,9 @@ defmodule EpochtalkServer.Models.Permission do
   import Ecto.Changeset
   alias EpochtalkServer.Repo
   alias EpochtalkServer.Models.Permission
+  @moduledoc """
+  `Permission` model, for performing actions relating to `Role` permissions, used for seeding
+  """
 
   @primary_key false
   schema "permissions" do
@@ -11,6 +14,13 @@ defmodule EpochtalkServer.Models.Permission do
 
   ## === Changesets Functions ===
 
+  @doc """
+  Creates a generic changeset for `Permission` model
+  """
+  @spec changeset(
+    permission :: %EpochtalkServer.Models.Permission{},
+    attrs :: %{} | nil
+  ) :: %EpochtalkServer.Models.Permission{}
   def changeset(permission, attrs \\ %{}) do
     permission
     |> cast(attrs, [:path])
@@ -19,7 +29,15 @@ defmodule EpochtalkServer.Models.Permission do
 
   ## === Database Functions ===
 
+  @doc """
+  Returns every `Permission` record in the database
+  """
+  @spec all() :: [%EpochtalkServer.Models.Permission{}] | []
   def all(), do: Repo.all(Permission)
 
+  @doc """
+  Returns a specific `Permission` provided it's path
+  """
+  @spec by_path(path :: String.t()) :: %EpochtalkServer.Models.Permission{} | nil
   def by_path(path) when is_binary(path), do: Repo.get_by(Permission, path: path)
 end

b5915d5b73171dc06995d8727d3fdf07b1d25530
Anthony Kinsey <anthony@slickage.com>
2022-10-06 14:23:21 -1000

    docs(board-mapping): add docs for board mapping model

--- lib/epochtalk_server/models/board_mapping.ex
+++ lib/epochtalk_server/models/board_mapping.ex
@@ -6,6 +6,9 @@ defmodule EpochtalkServer.Models.BoardMapping do
   alias EpochtalkServer.Models.Board
   alias EpochtalkServer.Models.BoardMapping
   alias EpochtalkServer.Models.Category
+  @moduledoc """
+  `BoardMapping` model, for performing actions relating to mapping forum boards and categories
+  """
 
   @primary_key false
   schema "board_mapping" do
@@ -17,6 +20,13 @@ defmodule EpochtalkServer.Models.BoardMapping do
 
   ## === Changesets Functions ===
 
+  @doc """
+  Create generic changeset for `BoardMapping` model
+  """
+  @spec changeset(
+    board_mapping :: %EpochtalkServer.Models.BoardMapping{},
+    attrs :: %{} | nil
+  ) :: %EpochtalkServer.Models.BoardMapping{}
   def changeset(board_mapping, attrs) do
     board_mapping
     |> cast(attrs, [:board_id, :parent_id, :category_id, :view_order])
@@ -27,23 +37,36 @@ defmodule EpochtalkServer.Models.BoardMapping do
 
   ## === Database Functions ===
 
-  def insert(%BoardMapping{} = board_mapping), do: Repo.insert(board_mapping)
-
-  def delete_board(id) do
+  @doc """
+  Deletes a `Board` from the `BoardMapping`
+  """
+  @spec delete_board_by_id(
+    board_id :: integer()
+  ) :: {non_neg_integer(), nil | [term()]}
+  def delete_board_by_id(id) when is_integer(id) do
     query = from bm in BoardMapping,
       where: bm.board_id == ^id
     Repo.delete_all(query)
   end
 
+  @doc """
+  Updates `BoardMapping` in the database
+  """
+  @spec update(
+    board_mapping_list :: [%{}]
+  ) :: {:ok, Ecto.Changeset.t()} | {:error, Ecto.Changeset.t()}| {:error, any()}
   def update(board_mapping_list) do
     Repo.transaction(fn ->
-      Enum.each(board_mapping_list, &BoardMapping.update(&1, Map.get(&1, :type)))
+      Enum.each(board_mapping_list, &update(&1, Map.get(&1, :type)))
     end)
   end
-  def update(cat, "category"), do: Category.update_for_board_mapping(cat)
-  def update(uncat, "uncategorized"), do: delete_board(Map.get(uncat, :id))
-  def update(board, "board") do
-    delete_board(Map.get(board, :id))
+
+  ## === Private Helper Functions ===
+
+  defp update(cat, "category"), do: Category.update_for_board_mapping(cat)
+  defp update(uncat, "uncategorized"), do: BoardMapping.delete_board_by_id(Map.get(uncat, :id))
+  defp update(board, "board") do
+    BoardMapping.delete_board_by_id(Map.get(board, :id))
     %BoardMapping{}
     |> changeset(Map.put(board, :board_id, Map.get(board, :id)))
     |> Repo.insert

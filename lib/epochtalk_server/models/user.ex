e3a34e177512d53ba35ef3c9a8f6d2746b9955b5
Anthony Kinsey <anthony@slickage.com>
2022-10-07 16:32:35 -1000

    docs(user): add docs to user model

--- lib/epochtalk_server/models/user.ex
+++ lib/epochtalk_server/models/user.ex
@@ -11,6 +11,9 @@ defmodule EpochtalkServer.Models.User do
   alias EpochtalkServer.Models.BannedAddress
   alias EpochtalkServer.Models.BoardModerator
   alias EpochtalkServer.Models.Ban
+  @moduledoc """
+  `User` model, for performing actions relating a user
+  """
 
   schema "users" do
     field :email, :string
@@ -38,6 +41,14 @@ defmodule EpochtalkServer.Models.User do
 
   ## === Changesets Functions ===
 
+  @doc """
+  Creates a registration changeset for `User` model, returns an error changeset
+  if validation of username, email and password do not pass.
+  """
+  @spec registration_changeset(
+    user :: %EpochtalkServer.Models.User{},
+    attrs :: %{} | nil
+  ) :: %EpochtalkServer.Models.User{}
   def registration_changeset(user, attrs) do
     user
     |> cast(attrs, [:id, :email, :username, :created_at, :updated_at, :deleted, :malicious_score, :password])
@@ -49,13 +60,12 @@ defmodule EpochtalkServer.Models.User do
 
   ## === Database Functions ===
 
-  # create admin, for seeding
-  def create(attrs, true = _admin) do
-    Repo.transaction(fn ->
-      {:ok, user} = create(attrs)
-      RoleUser.set_admin(user.id)
-    end)
-  end
+  @doc """
+  Creates a new `User` in the database, used for registration
+  """
+  @spec create(
+    attrs :: %{}
+  ) :: {:ok, user :: %EpochtalkServer.Models.User{}} | {:error, Ecto.Changeset.t()}
   def create(attrs) do
     user_cs = User.registration_changeset(%User{}, attrs)
     case Repo.insert(user_cs) do
@@ -68,14 +78,60 @@ defmodule EpochtalkServer.Models.User do
     end
   end
 
+  @doc """
+  Creates a new `User` in the database and assigns the `superAdministrator` `Role`, used for seeding
+  """
+  @spec create(
+    attrs :: %{},
+    admin :: boolean
+  ) :: {:ok, user :: %EpochtalkServer.Models.User{}} | {:error, Ecto.Changeset.t()}
+  def create(attrs, true = _admin) do
+    Repo.transaction(fn ->
+      {:ok, user} = create(attrs)
+      RoleUser.set_admin(user.id)
+    end)
+  end
+
+  @doc """
+  Checks if `User` with `username` exists in the database
+  """
+  @spec with_username_exists?(
+    username :: String.t()
+  ) :: true | false
   def with_username_exists?(username), do: Repo.exists?(from u in User, where: u.username == ^username)
 
+  @doc """
+  Checks if `User` with `email` exists in the database
+  """
+  @spec with_email_exists?(
+    email :: String.t()
+  ) :: true | false
   def with_email_exists?(email), do: Repo.exists?(from u in User, where: u.email == ^email)
 
+  @doc """
+  Gets a `User` from the database by `id`
+  """
+  @spec by_id(
+    id :: integer
+  ) :: %EpochtalkServer.Models.User{} | nil
   def by_id(id) when is_integer(id), do: Repo.get_by(User, id: id)
 
+  @doc """
+  Clears the malicious score of a `User` by `id`, from the database
+  """
+  @spec clear_malicious_score_by_id(
+    id :: integer
+  ) :: {non_neg_integer(), nil}
   def clear_malicious_score_by_id(id), do: set_malicious_score_by_id(id, nil)
 
+  @doc """
+  Gets a `User` by `username`, from the database, with all of it's associations preloaded.
+  Appends the `user` `Role` to `user.roles` if no roles present. Strips all roles but
+  `banned` from `user.roles` if user is banned.
+  """
+  @spec by_username(
+    username :: String.t()
+  ) :: {:ok, user :: %EpochtalkServer.Models.User{}} | {:error, :user_not_found}
   def by_username(username) when is_binary(username) do
     query = from u in User,
       where: u.username == ^username,
@@ -85,6 +141,16 @@ defmodule EpochtalkServer.Models.User do
       else: {:error, :user_not_found}
   end
 
+  @doc """
+  Checks if the provided `User` is malicious using the provided `ip`. If the `User`
+  is found to be malicious after checking `BannedAddress` records, the user's
+  `malicious_score` is updated and is assigned the `banned` `Role`, in the database
+  and in place. Otherwise the user is just returned with no change.
+  """
+  @spec handle_malicious_user(
+    user :: %EpochtalkServer.Models.User{},
+    ip :: tuple
+  ) :: {:ok, user :: %EpochtalkServer.Models.User{}} | {:error, :ban_error}
   def handle_malicious_user(%User{} = user, ip) do
     # convert ip tuple into string
     ip_str = ip |> :inet_parse.ntoa |> to_string
@@ -98,7 +164,16 @@ defmodule EpochtalkServer.Models.User do
       else: Ban.ban(user)
   end
 
-  def valid_password?(%{passhash: hashed_password} = _user, password)
+  ## === Helper Functions ===
+
+  @doc """
+  Validates with Argon2 that a `User` `passhash` matches the supplied `password`
+  """
+  @spec valid_password?(
+    user :: %EpochtalkServer.Models.User{},
+    password :: String.t()
+  ) :: true | false
+  def valid_password?(%User{passhash: hashed_password} = _user, password)
       when is_binary(hashed_password) and byte_size(password) > 0 do
     Argon2.verify_pass(password, hashed_password)
   end

7cd039b1f25c9688a6976dce4a871a6dfa34787c
Anthony Kinsey <anthony@slickage.com>
2022-10-11 12:54:26 -1000

    docs(session): add docs to session code

--- lib/epochtalk_server/session.ex
+++ lib/epochtalk_server/session.ex
@@ -1,11 +1,24 @@
 defmodule EpochtalkServer.Session do
+  @moduledoc """
+  Manages `User` sessions in Redis. Used by Auth related `User` actions.
+  """
   alias EpochtalkServer.Auth.Guardian
   alias EpochtalkServer.Models.User
 
-  # set user session id, timestamp, ttl
-  # log user in with Guardian to get token
-  # save user session info to redis
-  # returns {:ok, user, token and conn}
+  @doc """
+  Create session performs the following actions:
+  * Sets user's session id, timestamp, ttl
+  * Logs `User` in with Guardian to get token
+  * Saves `User` session info to redis (avatar, roles, moderating, ban info, etc)
+  * returns {:ok, user, token and conn}
+
+  - TODO(boka): Handle expiration of redis sessions (this is handled in guardian but not redis)
+  """
+  @spec create(
+    user :: %EpochtalkServer.Models.User{},
+    remember_me :: boolean,
+    conn :: Plug.Conn.t()
+  ) :: {:ok, user :: %EpochtalkServer.Models.User{}, encoded_token :: String.t(), conn :: Plug.Conn.t()}
   def create(%User{} = user, remember_me, conn) do
     datetime = NaiveDateTime.utc_now
     session_id = UUID.uuid1()
@@ -24,6 +37,47 @@ defmodule EpochtalkServer.Session do
     # return user with token
     {:ok, user, encoded_token, conn}
   end
+
+  @doc """
+  Gets all sessions for a specific `User`
+  """
+  @spec get_sessions(
+    user_id :: integer
+  ) :: {:ok, Redix.Protocol.redis_value()} | {:error, atom() | Redix.Error.t() | Redix.ConnectionError.t()}
+  def get_sessions(user_id) do
+    # get session id's from redis under "user:{user_id}:sessions"
+    session_key = generate_key(user_id, "sessions")
+    Redix.command(:redix, ["SMEMBERS", session_key])
+  end
+
+  @doc """
+  Deletes a specific `User` session
+  """
+  @spec delete_session(
+    user_id :: integer,
+    session_id :: String.t()
+  ) :: {:ok, Redix.Protocol.redis_value()} | {:error, atom() | Redix.Error.t() | Redix.ConnectionError.t()}
+  def delete_session(user_id, session_id) do
+    # delete session id from redis under "user:{user_id}:sessions"
+    session_key = generate_key(user_id, "sessions")
+    Redix.command(:redix, ["SREM", session_key, session_id])
+  end
+
+  @doc """
+  Deletes every session instance for the specified `User`
+  """
+  @spec delete_sessions(
+    user_id :: integer,
+    session_id :: String.t()
+  ) :: {:ok, Redix.Protocol.redis_value()} | {:error, atom() | Redix.Error.t() | Redix.ConnectionError.t()}
+  def delete_sessions(user_id, session_id) do
+    # delete session id from redis under "user:{user_id}:sessions"
+    session_key = generate_key(user_id, "sessions")
+    Redix.command(:redix, ["SPOP", session_key, session_id])
+    # repeat until redix returns nil
+    |> unless do delete_sessions(user_id, session_id) end
+  end
+
   defp save(%User{} = user, session_id) do
     # TODO: return role lookups from db instead of entire roles
     avatar = if is_nil(user.profile), do: nil, else: user.profile.avatar
@@ -35,8 +89,9 @@ defmodule EpochtalkServer.Session do
     update_moderating(user.id, user.moderating)
     set_session(user.id, session_id)
   end
+
   # use default role
-  def update_roles(user_id, roles) when is_list(roles) do
+  defp update_roles(user_id, roles) when is_list(roles) do
     # save/replace roles to redis under "user:{user_id}:roles"
     role_lookups = roles |> Enum.map(&(&1.lookup))
     role_key = generate_key(user_id, "roles")
@@ -44,7 +99,8 @@ defmodule EpochtalkServer.Session do
     unless role_lookups == nil or role_lookups == [], do:
       Enum.each(role_lookups, &Redix.command(:redix, ["SADD", role_key, &1]))
   end
-  def update_moderating(user_id, moderating) do
+
+  defp update_moderating(user_id, moderating) do
     # get list of board ids from user.moderating
     moderating = moderating |> Enum.map(&(&1.board_id))
     # save/replace moderating boards to redis under "user:{user_id}:moderating"
@@ -53,51 +109,37 @@ defmodule EpochtalkServer.Session do
     unless moderating == nil or moderating == [], do:
       Enum.each(moderating, &Redix.command(:redix, ["SADD", moderating_key, &1]))
   end
-  def update_user_info(user_id, username) do
+
+  defp update_user_info(user_id, username) do
     user_key = generate_key(user_id, "user")
     # delete avatar from redis hash under "user:{user_id}"
     Redix.command(:redix, ["HDEL", user_key, "avatar"])
     # save username to redis hash under "user:{user_id}"
     Redix.command(:redix, ["HSET", user_key, "username", username])
   end
-  def update_user_info(user_id, username, avatar) when is_nil(avatar) or avatar == "" do
+  defp update_user_info(user_id, username, avatar) when is_nil(avatar) or avatar == "" do
     update_user_info(user_id, username)
   end
-  def update_user_info(user_id, username, avatar) do
+  defp update_user_info(user_id, username, avatar) do
     # save username, avatar to redis hash under "user:{user_id}"
     user_key = generate_key(user_id, "user")
     Redix.command(:redix, ["HSET", user_key, "username", username, "avatar", avatar])
   end
-  def update_ban_info(user_id, ban_info) do
+
+  defp update_ban_info(user_id, ban_info) do
     # save/replace ban_expiration to redis under "user:{user_id}:baninfo"
     ban_key = generate_key(user_id, "baninfo")
     Redix.command(:redix, ["HDEL", ban_key, "ban_expiration", "malicious_score"])
     if ban_exp = Map.get(ban_info, :ban_expiration), do: Redix.command(:redix, ["HSET", ban_key, "ban_expiration", ban_exp])
     if malicious_score = Map.get(ban_info, :malicious_score), do: Redix.command(:redix, ["HSET", ban_key, "malicious_score", malicious_score])
   end
+
   defp set_session(user_id, session_id) do
     # save session id to redis under "user:{user_id}:sessions"
     session_key = generate_key(user_id, "sessions")
     Redix.command(:redix, ["SADD", session_key, session_id])
   end
-  defp get_sessions(user_id) do
-    # get session id's from redis under "user:{user_id}:sessions"
-    session_key = generate_key(user_id, "sessions")
-    Redix.command(:redix, ["SMEMBERS", session_key])
-  end
-  # TODO: invalidate token
-  defp delete_session(user_id, session_id) do
-    # delete session id from redis under "user:{user_id}:sessions"
-    session_key = generate_key(user_id, "sessions")
-    Redix.command(:redix, ["SREM", session_key, session_id])
-  end
-  defp delete_sessions(user_id, session_id) do
-    # delete session id from redis under "user:{user_id}:sessions"
-    session_key = generate_key(user_id, "sessions")
-    Redix.command(:redix, ["SPOP", session_key, session_id])
-    # repeat until redix returns nil
-    |> unless do delete_sessions(user_id, session_id) end
-  end
+
   defp generate_key(user_id, "user"), do: "user:#{user_id}"
   defp generate_key(user_id, type), do: "user:#{user_id}:#{type}"
 end

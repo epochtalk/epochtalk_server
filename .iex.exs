6a66edc7b51227d515d9fb221b4b526a1dd723d5
Anthony Kinsey <anthony@slickage.com>
2022-10-17 13:52:39 -1000

    refactor(.iex.exs): modify aliases of models

--- .iex.exs
+++ .iex.exs
@@ -3,20 +3,23 @@ IEx.configure(inspect: [limit: :infinity])
 
 alias EpochtalkServer.Repo
 
-alias EpochtalkServer.Models.Ban
-alias EpochtalkServer.Models.BannedAddress
-alias EpochtalkServer.Models.Board
-alias EpochtalkServer.Models.BoardMapping
-alias EpochtalkServer.Models.BoardModerator
-alias EpochtalkServer.Models.Category
-alias EpochtalkServer.Models.Invitation
-alias EpochtalkServer.Models.MetadataBoard
-alias EpochtalkServer.Models.Permission
-alias EpochtalkServer.Models.Preference
-alias EpochtalkServer.Models.Profile
-alias EpochtalkServer.Models.Role
-alias EpochtalkServer.Models.RolePermission
-alias EpochtalkServer.Models.RoleUser
-alias EpochtalkServer.Models.User
+alias EpochtalkServer.Models.{
+  Ban,
+  BannedAddress,
+  Board,
+  BoardMapping,
+  BoardModerator,
+  Category,
+  Configuration,
+  Invitation,
+  MetadataBoard,
+  Permission,
+  Preference,
+  Profile,
+  Role,
+  RolePermission,
+  RoleUser,
+  User
+}
 
 reload = fn() -> r Enum.map(__ENV__.aliases, fn {_, module} -> module end) end

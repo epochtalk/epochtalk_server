1c25af3147ba20a67325340e59ec84a94ea95018
Anthony Kinsey <anthony@slickage.com>
2023-01-19 15:43:29 -1000

    test(acl): implement more detailed testing

--- test/epochtalk_server_web/helpers/acl_test.exs
+++ test/epochtalk_server_web/helpers/acl_test.exs
@@ -14,6 +14,20 @@ defmodule EpochtalkServerWeb.ACLTest do
       assert ACL.allow!(user, "threads.create.allow") == :ok
     end
 
+    test "raises 'InvalidPermissions' error with unauthenticated connection" do
+      assert_raise InvalidPermission,
+                   ~r/^Forbidden, invalid permissions to perform this action/,
+                   fn ->
+                     ACL.allow!(%Plug.Conn{}, "posts.create")
+                   end
+
+      assert_raise InvalidPermission,
+                   ~r/^Forbidden, invalid permissions to perform this action/,
+                   fn ->
+                     ACL.allow!(%Plug.Conn{}, "threads.create")
+                   end
+    end
+
     @tag :authenticated
     test "raises 'InvalidPermissions' error with invalid 'User' role permissions", %{user: user} do
       assert_raise InvalidPermission,
@@ -33,6 +47,31 @@ defmodule EpochtalkServerWeb.ACLTest do
                    fn ->
                      ACL.allow!(user, "adminSettings.find")
                    end
+    end
+  end
+
+  describe "allow!/3" do
+    @tag :authenticated
+    test "returns :ok with valid 'User' role permissions", %{user: user} do
+      assert ACL.allow!(user, "boards.allCategories", "You cannot query all categories") == :ok
+      assert ACL.allow!(user, "posts.create", "You cannot create posts") == :ok
+      assert ACL.allow!(user, "threads.create", "You cannot create threads") == :ok
+
+      assert ACL.allow!(user, "boards.allCategories.allow", "You cannot query all categories") ==
+               :ok
+
+      assert ACL.allow!(user, "posts.create.allow", "You cannot create posts") == :ok
+      assert ACL.allow!(user, "threads.create.allow", "You cannot create threads") == :ok
+    end
+
+    test "raises 'InvalidPermissions' error with unauthenticated connection" do
+      assert_raise InvalidPermission, ~r/^You cannot create posts/, fn ->
+        ACL.allow!(%Plug.Conn{}, "posts.create", "You cannot create posts")
+      end
+
+      assert_raise InvalidPermission, ~r/^You cannot create threads/, fn ->
+        ACL.allow!(%Plug.Conn{}, "threads.create", "You cannot create threads")
+      end
     end
 
     @tag :authenticated

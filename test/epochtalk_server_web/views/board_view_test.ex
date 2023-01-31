29d0989227f9b04537cfecaa2d6b570b445da870
Anthony Kinsey <anthony@slickage.com>
2023-01-30 15:26:25 -1000

    feat(boards-view): add board view and test skeletons

---
+++ test/epochtalk_server_web/views/board_view_test.ex
@@ -0,0 +1,7 @@
+defmodule EpochtalkServerWeb.BoardViewTest do
+  use EpochtalkServerWeb.ConnCase, async: true
+  alias EpochtalkServerWeb.BoardView
+
+  # Specify that we want to use doctests:
+  doctest BoardView
+end

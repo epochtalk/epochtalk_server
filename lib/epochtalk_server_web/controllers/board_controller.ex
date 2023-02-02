44af06bd808daecab42d53ebaf9cf5a302dae28f
Anthony Kinsey <anthony@slickage.com>
2023-02-02 09:57:35 -1000

    refactor(boards): wip implement by_category for boards controller

--- lib/epochtalk_server_web/controllers/board_controller.ex
+++ lib/epochtalk_server_web/controllers/board_controller.ex
@@ -5,21 +5,25 @@ defmodule EpochtalkServerWeb.BoardController do
   Controller For `Board` related API requests
   """
   alias EpochtalkServer.Auth.Guardian
-  alias EpochtalkServer.Models.Board
+  # alias EpochtalkServer.Models.Board
   alias EpochtalkServerWeb.ErrorHelpers
+  alias EpochtalkServerWeb.Helpers.Validate
   alias EpochtalkServerWeb.Helpers.ACL
 
   @doc """
   Used to retrieve categorized boards
   """
-  def by_category(conn, _attrs) do
-    with {:auth, user} <- {:auth, Guardian.Plug.current_resource(conn)} do
-      priority = ACL.get_user_priority(user)
-      IO.inspect user
-      render(conn, "categorized.json", data: priority)
+  def by_category(conn, attrs) do
+    with page <- Validate.cast(attrs, "page", :integer, min: 1, default: 1),
+         limit <- Validate.cast(attrs, "limit", :integer, min: 1, max: 100, default: 5),
+         stripped <- Validate.cast(attrs, "stripped", :boolean),
+         user <- Guardian.Plug.current_resource(conn),
+         priority <- ACL.get_user_priority(user) do
+         IO.inspect user
+      opts = %{page: page, limit: limit, stripped: stripped}
+      render(conn, "categorized.json", data: %{opts: opts, user_id: user.id, priority: priority})
     else
-      {:auth, nil} ->
-        ErrorHelpers.render_json_error(conn, 400, "Error, cannot fetch boards")
+      _ -> ErrorHelpers.render_json_error(conn, 400, "Error, cannot fetch boards")
     end
   end
 end

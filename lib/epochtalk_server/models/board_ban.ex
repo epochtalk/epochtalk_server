ba609b55a9ab30f3853b5d01062a534daf2bef8b
Anthony Kinsey <anthony@slickage.com>
2023-02-14 15:30:57 -1000

    feat(board-bans): wip implement db function to check if user is board banned, todo: add ability to check from post and thread

--- lib/epochtalk_server/models/board_ban.ex
+++ lib/epochtalk_server/models/board_ban.ex
@@ -1,8 +1,10 @@
 defmodule EpochtalkServer.Models.BoardBan do
   use Ecto.Schema
+  import Ecto.Query
   import Ecto.Changeset
   alias EpochtalkServer.Repo
   alias EpochtalkServer.Models.User
+  alias EpochtalkServer.Models.BoardBan
   alias EpochtalkServer.Models.Board
 
   @moduledoc """
@@ -34,5 +36,13 @@ defmodule EpochtalkServer.Models.BoardBan do
     |> validate_required([:user_id, :board_id])
   end
 
-  def all(), do: Repo.all(BoardBan)
+  def is_banned_from_board(user_id, opts) do
+    board_id = Keyword.get(opts, :board_id)
+    query = from bb in BoardBan,
+      where: bb.user_id == ^user_id and bb.board_id == ^board_id,
+      select: bb.user_id
+    if board_id, do: is_integer(Repo.one(query)), else: {:error, :board_does_not_exist}
+  end
+
+  def is_not_banned_from_board(user_id, opts), do: !is_banned_from_board(user_id, opts)
 end

ff226d1a9324fababb35dc9114c70bb5ebe2e71c
unenglishable <unenglishable@gmail.com>
2023-02-16 21:46:15 -0800

    test(session): handle ttl test 2

--- test/epochtalk_server/session_test.exs
+++ test/epochtalk_server/session_test.exs
@@ -81,23 +81,23 @@ defmodule EpochtalkServerWeb.SessionTest do
       assert baninfo_ttl_1 <= @one_day_in_seconds
       assert sessions_ttl_1 > @almost_one_day_in_seconds
       assert sessions_ttl_1 <= @one_day_in_seconds
-      #
-      # remember_me = true
-      # {:ok, authed_user, _token, _authed_conn} = Session.create(user, remember_me, conn)
-      # user_ttl = Redix.command!(:redix, ["TTL", "user:#{authed_user.id}"])
-      # roles_ttl = Redix.command!(:redix, ["TTL", "user:#{authed_user.id}:roles"])
-      # moderating_ttl = Redix.command!(:redix, ["TTL", "user:#{authed_user.id}:moderating"])
-      # baninfo_ttl = Redix.command!(:redix, ["TTL", "user:#{authed_user.id}:baninfo"])
-      # sessions_ttl = Redix.command!(:redix, ["TTL", "user:#{authed_user.id}:sessions"])
-      #
-      # assert user_ttl > @almost_four_weeks_in_seconds
-      # assert user_ttl <= @four_weeks_in_seconds
-      # assert roles_ttl > @almost_four_weeks_in_seconds
-      # assert roles_ttl <= @four_weeks_in_seconds
-      # assert moderating_ttl <= @four_weeks_in_seconds
-      # assert baninfo_ttl <= @four_weeks_in_seconds
-      # assert sessions_ttl > @almost_four_weeks_in_seconds
-      # assert sessions_ttl <= @four_weeks_in_seconds
+
+      remember_me_2 = true
+      {:ok, authed_user_2, _token, _authed_conn} = Session.create(user, remember_me_2, conn)
+      user_ttl_2 = Redix.command!(:redix, ["TTL", "user:#{authed_user_2.id}"])
+      roles_ttl_2 = Redix.command!(:redix, ["TTL", "user:#{authed_user_2.id}:roles"])
+      moderating_ttl_2 = Redix.command!(:redix, ["TTL", "user:#{authed_user_2.id}:moderating"])
+      baninfo_ttl_2 = Redix.command!(:redix, ["TTL", "user:#{authed_user_2.id}:baninfo"])
+      sessions_ttl_2 = Redix.command!(:redix, ["TTL", "user:#{authed_user_2.id}:sessions"])
+
+      assert user_ttl_2 > @almost_four_weeks_in_seconds
+      assert user_ttl_2 <= @four_weeks_in_seconds
+      assert roles_ttl_2 > @almost_four_weeks_in_seconds
+      assert roles_ttl_2 <= @four_weeks_in_seconds
+      assert moderating_ttl_2 <= @four_weeks_in_seconds
+      assert baninfo_ttl_2 <= @four_weeks_in_seconds
+      assert sessions_ttl_2 > @almost_four_weeks_in_seconds
+      assert sessions_ttl_2 <= @four_weeks_in_seconds
       #
       # remember_me = false
       # {:ok, authed_user, _token, _authed_conn} = Session.create(user, remember_me, conn)

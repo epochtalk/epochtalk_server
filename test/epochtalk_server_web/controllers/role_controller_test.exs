cb43fa3af6a663b767f307a72bac7a52150a3594
unenglishable <unenglishable@gmail.com>
2023-03-02 15:31:07 -0800

    refactor(mix): format

--- test/epochtalk_server_web/controllers/role_controller_test.exs
+++ test/epochtalk_server_web/controllers/role_controller_test.exs
@@ -6,9 +6,24 @@ defmodule EpochtalkServerWeb.RoleControllerTest do
     test "gets all roles when authenticated", %{conn: conn} do
       conn = get(conn, Routes.role_path(conn, :all))
       roles = json_response(conn, 200)
-      assert [%{"lookup" => "superAdministrator", "name" => "Super Administrator", "priority" => 0} | roles] = roles
-      assert [%{"lookup" => "administrator", "name" => "Administrator", "priority" => 1} | roles] = roles
-      assert [%{"lookup" => "globalModerator", "name" => "Global Moderator", "priority" => 2} | roles] = roles
+
+      assert [
+               %{
+                 "lookup" => "superAdministrator",
+                 "name" => "Super Administrator",
+                 "priority" => 0
+               }
+               | roles
+             ] = roles
+
+      assert [%{"lookup" => "administrator", "name" => "Administrator", "priority" => 1} | roles] =
+               roles
+
+      assert [
+               %{"lookup" => "globalModerator", "name" => "Global Moderator", "priority" => 2}
+               | roles
+             ] = roles
+
       assert [%{"lookup" => "moderator", "name" => "Moderator", "priority" => 3} | roles] = roles
       assert [%{"lookup" => "user", "name" => "User", "priority" => 4} | roles] = roles
       assert [%{"lookup" => "patroller", "name" => "Patroller", "priority" => 5} | roles] = roles
@@ -21,7 +36,9 @@ defmodule EpochtalkServerWeb.RoleControllerTest do
 
     test "does not get roles when not authenticated", %{conn: conn} do
       conn = get(conn, Routes.role_path(conn, :all))
-      assert %{"error" => "Unauthorized", "message" => "No resource found"} = json_response(conn, 401)
+
+      assert %{"error" => "Unauthorized", "message" => "No resource found"} =
+               json_response(conn, 401)
     end
   end
 end

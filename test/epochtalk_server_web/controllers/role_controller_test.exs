7ba444896b140297088e1e317d3030913e756957
unenglishable <unenglishable@gmail.com>
2023-03-02 14:59:30 -0800

    test(role controller): add role controller test

---
+++ test/epochtalk_server_web/controllers/role_controller_test.exs
@@ -0,0 +1,21 @@
+defmodule EpochtalkServerWeb.RoleControllerTest do
+  use EpochtalkServerWeb.ConnCase, async: false
+
+  describe "all/2" do
+    @tag :authenticated
+    test "gets all roles when authenticated", %{conn: conn} do
+      conn = get(conn, Routes.role_path(conn, :all))
+      assert [%{"lookup" => "superAdministrator", "name" => "Super Administrator", "priority" => 0} | roles] = json_response(conn, 200)
+      assert [%{"lookup" => "administrator", "name" => "Administrator", "priority" => 1} | roles] = roles
+      assert [%{"lookup" => "globalModerator", "name" => "Global Moderator", "priority" => 2} | roles] = roles
+      assert [%{"lookup" => "moderator", "name" => "Moderator", "priority" => 3} | roles] = roles
+      assert [%{"lookup" => "user", "name" => "User", "priority" => 4} | roles] = roles
+      assert [%{"lookup" => "patroller", "name" => "Patroller", "priority" => 5} | roles] = roles
+      assert [%{"lookup" => "newbie", "name" => "Newbie", "priority" => 6} | roles] = roles
+      assert [%{"lookup" => "banned", "name" => "Banned", "priority" => 7} | roles] = roles
+      assert [%{"lookup" => "anonymous", "name" => "Anonymous", "priority" => 8} | roles] = roles
+      assert [%{"lookup" => "private", "name" => "Private", "priority" => 9} | roles] = roles
+      assert [] = roles
+    end
+  end
+end

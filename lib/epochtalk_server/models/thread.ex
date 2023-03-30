ddee8057c0294328a1b0ec2042aef929adf19633
Anthony Kinsey <anthony@slickage.com>
2023-03-30 11:09:08 -1000

    feat(thread-set-timestamps): port db function to set threads created_at and updated_at timestamps

--- lib/epochtalk_server/models/thread.ex
+++ lib/epochtalk_server/models/thread.ex
@@ -124,6 +124,40 @@ defmodule EpochtalkServer.Models.Thread do
     end
   end
 
+
+  @doc """
+  Sets the `created_at` and `updated_at` of a new `Thread` by querying it's posts.
+  """
+  @spec set_timestamps(id :: non_neg_integer) :: {non_neg_integer(), nil}
+  def set_timestamps(id) do
+    # get created_at of first post in thread
+    created_at_query = from p in Post,
+      where: p.thread_id == ^id,
+      order_by: [p.created_at],
+      limit: 1,
+      select: p.created_at
+
+    created_at = Repo.one(created_at_query)
+
+    # get created_at of last post in thread
+    updated_at_query = from p in Post,
+      where: p.thread_id == ^id,
+      order_by: [desc: p.created_at],
+      limit: 1,
+      select: p.created_at
+
+    updated_at = Repo.one(updated_at_query)
+
+    # set created_at and updated_at of thread
+    from(t in Thread, where: t.id == ^id)
+    |> Repo.update_all(
+      set: [
+        created_at: created_at,
+        updated_at: updated_at
+      ]
+    )
+  end
+
   @doc """
   Returns recent threads accounting for user priority and user's ignored boards
 

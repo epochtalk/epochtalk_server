a5f7eeeb0e8562ca2923d4ac64d8a8b4a42e1a8d
Anthony Kinsey <anthony@slickage.com>
2023-03-31 14:17:03 -1000

    feat(post-create): implement database function for post create

--- lib/epochtalk_server/models/post.ex
+++ lib/epochtalk_server/models/post.ex
@@ -1,10 +1,12 @@
 defmodule EpochtalkServer.Models.Post do
   use Ecto.Schema
   import Ecto.Changeset
-  # alias EpochtalkServer.Repo
-  # alias EpochtalkServer.Models.Post
+  import Ecto.Query
+  alias EpochtalkServer.Repo
+  alias EpochtalkServer.Models.Post
   alias EpochtalkServer.Models.Thread
   alias EpochtalkServer.Models.User
+  alias EpochtalkServer.Models.Profile
 
   @moduledoc """
   `Post` model, for performing actions relating to forum posts
@@ -68,10 +70,13 @@ defmodule EpochtalkServer.Models.Post do
   def create_changeset(post, attrs) do
     now = NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
 
-    attrs =
-      attrs
-      |> Map.put("created_at", now)
-      |> Map.put("updated_at", now)
+    # set default values and timestamps
+    post =
+      post
+      |> Map.put(:deleted, false)
+      |> Map.put(:locked, false)
+      |> Map.put(:created_at, now)
+      |> Map.put(:updated_at, now)
 
     post
     |> cast(attrs, [
@@ -83,8 +88,45 @@ defmodule EpochtalkServer.Models.Post do
       :created_at,
       :updated_at
     ])
+    |> validate_required([:user_id, :thread_id, :content])
     |> unique_constraint(:id, name: :posts_pkey)
     |> foreign_key_constraint(:thread_id, name: :posts_thread_id_fkey)
+  end
+
+  ## === Database Functions ===
+
+  @doc """
+  Creates a new `Post` in the database
+  """
+  @spec create(post_attrs :: map()) :: {:ok, post :: t()} | {:error, Ecto.Changeset.t()}
+  def create(post) do
+    Repo.transaction(fn ->
+      post_cs = create_changeset(%Post{}, post)
+
+      case Repo.insert(post_cs) do
+        # changeset valid, insert success, update metadata threads and return thread
+        {:ok, db_post} ->
+
+          # Increment user post count
+          Profile.increment_post_count(db_post.user_id)
+
+          # Set thread created_at and updated_at
+          Thread.set_timestamps(db_post.thread_id)
+
+          # Set post position
+          Post.set_position_using_thread(db_post.id, db_post.thread_id)
+
+          # Increment thread post ocunt
+          Thread.increment_post_count(db_post.thread_id)
+
+          # Requery post with position and thread slug info
+          Repo.one(from p in Post, where: p.id == ^db_post.id, preload: [:thread])
+
+        # changeset error
+        {:error, err} ->
+          err
+      end
+    end)
   end
 
   @doc """

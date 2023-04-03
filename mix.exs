8fd4fb5775178edb75b666ac2f067152fb6e13d6
unenglishable <unenglishable@gmail.com>
2023-04-03 13:12:44 -0700

    build(mix): update dialyxir

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule EpochtalkServer.MixProject do
       {:argon2_elixir, "~> 3.0.0"},
       {:corsica, "~> 1.2.0"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev], runtime: false},
       {:ecto_sql, "~> 3.6"},
       {:ex_doc, "~> 0.28.5"},
       {:gen_smtp, "~> 1.2"},

3ae299f991971220a2d812d47127bc776c7cdda9
unenglishable <unenglishable@gmail.com>
2023-04-11 11:30:29 -0700

    style(mix): format

--- test/epochtalk_server/session_test.exs
+++ test/epochtalk_server/session_test.exs
@@ -59,45 +59,76 @@ defmodule EpochtalkServerWeb.SessionTest do
     test "deletes an expired user session when logging in", %{conn: conn, user: user} do
       remember_me = false
       # create session that should be deleted
-      {:ok, authed_user_to_delete, _token, authed_conn_to_delete} = Session.create(user, remember_me, conn)
+      {:ok, authed_user_to_delete, _token, authed_conn_to_delete} =
+        Session.create(user, remember_me, conn)
+
       session_id_to_delete = authed_conn_to_delete.private.guardian_default_claims["jti"]
       # create session that shouldn't be deleted
-      {:ok, authed_user_to_persist, _token, authed_conn_to_persist} = Session.create(user, remember_me, conn)
+      {:ok, authed_user_to_persist, _token, authed_conn_to_persist} =
+        Session.create(user, remember_me, conn)
+
       session_id_to_persist = authed_conn_to_persist.private.guardian_default_claims["jti"]
       # check that all sessions are active
       {:ok, resource_to_delete} = Session.get_resource(user.id, session_id_to_delete)
-      %{id: session_user_id_to_delete, username: session_user_username_to_delete} = resource_to_delete
+
+      %{id: session_user_id_to_delete, username: session_user_username_to_delete} =
+        resource_to_delete
+
       assert session_user_id_to_delete == user.id
       assert session_user_username_to_delete == user.username
       {:ok, resource_to_persist} = Session.get_resource(user.id, session_id_to_persist)
-      %{id: session_user_id_to_persist, username: session_user_username_to_persist} = resource_to_persist
+
+      %{id: session_user_id_to_persist, username: session_user_username_to_persist} =
+        resource_to_persist
+
       assert session_user_id_to_persist == user.id
       assert session_user_username_to_persist == user.username
       # change expiration of session to delete to UTC 0
       expiration_utc = 0
-      Redix.command(:redix, ["ZADD", "user:#{user.id}:sessions", expiration_utc, session_id_to_delete])
+
+      Redix.command(:redix, [
+        "ZADD",
+        "user:#{user.id}:sessions",
+        expiration_utc,
+        session_id_to_delete
+      ])
+
       # create a new session (should delete expired sessions)
       {:ok, new_authed_user, _token, new_authed_conn} = Session.create(user, remember_me, conn)
       new_session_id = new_authed_conn.private.guardian_default_claims["jti"]
       # check that active sessions are still active
       {:ok, resource_to_persist} = Session.get_resource(user.id, session_id_to_persist)
-      %{id: session_user_id_to_persist, username: session_user_username_to_persist} = resource_to_persist
+
+      %{id: session_user_id_to_persist, username: session_user_username_to_persist} =
+        resource_to_persist
+
       assert session_user_id_to_persist == user.id
       assert session_user_username_to_persist == user.username
-      authenticate_persisted_conn = get(authed_conn_to_persist, Routes.user_path(authed_conn_to_persist, :authenticate))
+
+      authenticate_persisted_conn =
+        get(authed_conn_to_persist, Routes.user_path(authed_conn_to_persist, :authenticate))
+
       assert user.id == json_response(authenticate_persisted_conn, 200)["id"]
       {:ok, new_resource} = Session.get_resource(user.id, new_session_id)
       %{id: new_session_user_id, username: new_session_user_username} = new_resource
       assert new_session_user_id == user.id
       assert new_session_user_username == user.username
-      new_authenticate_conn = get(new_authed_conn, Routes.user_path(new_authed_conn, :authenticate))
+
+      new_authenticate_conn =
+        get(new_authed_conn, Routes.user_path(new_authed_conn, :authenticate))
+
       assert user.id == json_response(new_authenticate_conn, 200)["id"]
       # check that expired session is not active
       unauthed_resource = Session.get_resource(user.id, session_id_to_delete)
+
       assert unauthed_resource ==
                {:error, "No session for user_id #{user.id} with id #{session_id_to_delete}"}
-      authenticate_deleted_conn = get(authed_conn_to_delete, Routes.user_path(authed_conn_to_delete, :authenticate))
-      assert %{"error" => "Unauthorized", "message" => "No resource found"} = json_response(authenticate_deleted_conn, 401)
+
+      authenticate_deleted_conn =
+        get(authed_conn_to_delete, Routes.user_path(authed_conn_to_delete, :authenticate))
+
+      assert %{"error" => "Unauthorized", "message" => "No resource found"} =
+               json_response(authenticate_deleted_conn, 401)
     end
   end
 

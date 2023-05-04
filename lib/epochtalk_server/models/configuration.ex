e2e6b4d78356018e033626ab9aefcfd037272b6d
unenglishable <unenglishable@gmail.com>
2023-05-04 14:37:52 -0700

    docs(models/configuration): remove todo, fix spelling

--- lib/epochtalk_server/models/configuration.ex
+++ lib/epochtalk_server/models/configuration.ex
@@ -126,9 +126,7 @@ defmodule EpochtalkServer.Models.Configuration do
     {hash, _} = System.cmd("git", ["rev-parse", "--short", "HEAD"])
     [hash | _] = hash |> String.split("\n")
 
-    # TODO(boka): directory release version
-
-    # tag takes precidence over revision
+    # tag takes precedence over revision
     revision = if tag == "", do: hash, else: tag
 
     frontend_config =

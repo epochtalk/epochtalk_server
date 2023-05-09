abae17ca42bc2e5bb665aea65d907e3d9e10fffd
unenglishable <unenglishable@gmail.com>
2023-05-09 09:13:27 -1000

    feat(cache/role): implement cast for reloading roles

--- lib/epochtalk_server/cache/role.ex
+++ lib/epochtalk_server/cache/role.ex
@@ -24,12 +24,23 @@ defmodule EpochtalkServer.Cache.Role do
     {:reply, Map.get(role_cache, lookup), role_cache}
   end
 
+  @impl true
+  def handle_cast(:reload, _role_cache) do
+    role_cache = load()
+    {:noreply, role_cache}
+  end
+
   def start_link(_opts) do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def by_lookup(lookup_or_lookups) do
     GenServer.call(__MODULE__, {:lookup, lookup_or_lookups})
+  end
+
+  # reloads role cache
+  def reload() do
+    GenServer.cast(__MODULE__, :reload)
   end
 
   # returns loaded role cache

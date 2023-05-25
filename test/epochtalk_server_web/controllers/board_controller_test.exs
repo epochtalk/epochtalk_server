cc625d672e4d2ffeb69dfb92efe708331654383d
unenglishable <unenglishable@gmail.com>
2023-05-25 13:57:10 -1000

    test(controllers/board): move setup outside of describe
    don't continuously create more boards to test

--- test/epochtalk_server_web/controllers/board_controller_test.exs
+++ test/epochtalk_server_web/controllers/board_controller_test.exs
@@ -3,21 +3,21 @@ defmodule EpochtalkServerWeb.BoardControllerTest do
   alias EpochtalkServer.Models.Board
   alias EpochtalkServer.Repo
 
-  describe "find/2" do
-    setup %{conn: conn} do
-      # create board for testing
-      board = %{
-        name: "test board",
-        slug: "test-board",
-        description: "test board description",
-        viewable_by: 10,
-        postable_by: 10,
-        right_to_left: false
-      }
-      {:ok, new_board} = Board.create(board)
-      {:ok, conn: conn, board: new_board, board_attrs: board}
-    end
+  setup %{conn: conn} do
+    # create board for testing
+    board = %{
+      name: "test board",
+      slug: "test-board",
+      description: "test board description",
+      viewable_by: 10,
+      postable_by: 10,
+      right_to_left: false
+    }
+    {:ok, new_board} = Board.create(board)
+    {:ok, conn: conn, board: new_board, board_attrs: board}
+  end
 
+  describe "find/2" do
     test "does not find a board that doesn't exit", %{conn: conn} do
       result = conn
       |> get(Routes.board_path(conn, :find, 0))

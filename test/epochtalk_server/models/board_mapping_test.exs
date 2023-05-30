1cb7be0fd8e4c78c44e8172a95fe88abf6fcd5c1
unenglishable <unenglishable@gmail.com>
2023-05-30 12:28:17 -0700

    test(models/board_mapping): add board mapping test

---
+++ test/epochtalk_server/models/board_mapping_test.exs
@@ -0,0 +1,56 @@
+defmodule EpochtalkServer.BoardMappingTest do
+  use EpochtalkServer.DataCase, async: false
+  alias EpochtalkServer.Models.Category
+  alias EpochtalkServer.Models.Board
+  alias EpochtalkServer.Models.BoardMapping
+
+  setup _context do
+    # create category for testing
+    category_attrs = %{
+      name: "test category"
+    }
+
+    {:ok, category} = Category.create(category_attrs)
+
+    # create board for testing
+    board_attrs = %{
+      name: "test board",
+      slug: "test-board",
+      description: "test board description",
+      viewable_by: 10,
+      postable_by: 10,
+      right_to_left: false
+    }
+
+    {:ok, board} = Board.create(board_attrs)
+
+    {:ok,
+     board: board,
+     board_attrs: board_attrs,
+     category: category,
+     category_attrs: category_attrs}
+  end
+
+  describe "update/1" do
+    test "updates the board mapping", %{category: category, board: board} do
+      board_mapping_attrs = [
+        %{
+          id: category.id,
+          name: category.name,
+          type: "category",
+          view_order: 0
+        },
+        %{
+          id: board.id,
+          name: board.name,
+          type: "board",
+          category_id: category.id,
+          view_order: 1
+        }
+      ]
+
+      result = BoardMapping.update(board_mapping_attrs)
+      assert result == {:ok, :ok}
+    end
+  end
+end

7fdaf45bfbd1ebb0d813f74ce047665fc719735e
Anthony Kinsey <anthony@slickage.com>
2023-06-01 11:42:19 -1000

    refactor(posts-by-thread): wip implement post by thread route

--- lib/epochtalk_server_web/controllers/post_controller.ex
+++ lib/epochtalk_server_web/controllers/post_controller.ex
@@ -10,10 +10,10 @@ defmodule EpochtalkServerWeb.PostController do
   alias EpochtalkServerWeb.Helpers.ACL
   alias EpochtalkServer.Models.Post
   alias EpochtalkServer.Models.Thread
-  # alias EpochtalkServer.Models.Board
-  # alias EpochtalkServer.Models.BoardBan
-  # alias EpochtalkServer.Models.BoardMapping
-  # alias EpochtalkServer.Models.BoardModerator
+  alias EpochtalkServer.Models.Board
+  alias EpochtalkServer.Models.BoardBan
+  alias EpochtalkServer.Models.BoardMapping
+  alias EpochtalkServer.Models.BoardModerator
 
 
   # @doc """
@@ -40,8 +40,23 @@ defmodule EpochtalkServerWeb.PostController do
          page <- Validate.cast(attrs, "page", :integer, default: 1),
          start <- Validate.cast(attrs, "start", :string),
          limit <- Validate.cast(attrs, "limit", :integer, default: 25),
+         desc <- Validate.cast(attrs, "desc", :boolean, default: true),
          user <- Guardian.Plug.current_resource(conn),
-         user_priority <- ACL.get_user_priority(conn) do
+         user_priority <- ACL.get_user_priority(conn),
+         :ok <- ACL.allow!(conn, "posts.byThread"),
+         {:can_read, {:ok, true}} <-
+           {:can_read, Board.get_read_access_by_thread_id(thread_id, user_priority)},
+         {:ok, write_access} <- Board.get_write_access_by_thread_id(thread_id, user_priority),
+         {:ok, board_banned} <- BoardBan.is_banned_from_board(user, thread_id: thread_id),
+         board_mapping <- BoardMapping.all(),
+         board_moderators <- BoardModerator.all(),
+         threads <-
+           Post.page_by_thread_id(thread_id, page,
+             user: user,
+             per_page: limit,
+             field: field,
+             desc: desc
+           ) do
       # render(conn, :by_thread, %{
       #   posts: posts,
       #   write_access: write_access && is_map(user) && !board_banned,
@@ -57,11 +72,11 @@ defmodule EpochtalkServerWeb.PostController do
       #   desc: desc
       # })
     else
-      # {:error, :board_does_not_exist} ->
-      #   ErrorHelpers.render_json_error(conn, 400, "Error, thread does not exist")
+      {:error, :board_does_not_exist} ->
+        ErrorHelpers.render_json_error(conn, 400, "Error, thread does not exist")
 
-      # {:can_read, {:ok, false}} ->
-      #   ErrorHelpers.render_json_error(conn, 403, "Unauthorized, you do not have permission")
+      {:can_read, {:ok, false}} ->
+        ErrorHelpers.render_json_error(conn, 403, "Unauthorized, you do not have permission")
 
       _ ->
         ErrorHelpers.render_json_error(conn, 400, "Error, cannot get posts by thread")

e9f59f9ebeb3521281f391beeb0409f15f421017
unenglishable <unenglishable@gmail.com>
2023-06-02 10:01:26 -0700

    test(controllers/thread): add thread test

---
+++ test/epochtalk_server_web/controllers/thread_controller_test.exs
@@ -0,0 +1,16 @@
+defmodule EpochtalkServerWeb.ThreadControllerTest do
+  use EpochtalkServerWeb.ConnCase, async: false
+  alias EpochtalkServer.Models.Thread
+
+  describe "by_board/2" do
+    test "does not get threads for board that does not exist", %{conn: conn} do
+      result =
+        conn
+        |> get(Routes.thread_path(conn, :by_board), %{board_id: 3})
+        |> json_response(400)
+
+      assert %{"error" => "Bad Request"} = result
+      assert %{"message" => "Error, board does not exist"} = result
+    end
+  end
+end

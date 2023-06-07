403fd6d0f491e89725ac5f0b4e0bbf6cf2298235
Anthony Kinsey <anthony@slickage.com>
2023-06-06 14:21:31 -1000

    refactor(post-view): pass board data through formatter

--- lib/epochtalk_server_web/json/post_json.ex
+++ lib/epochtalk_server_web/json/post_json.ex
@@ -1,4 +1,5 @@
 defmodule EpochtalkServerWeb.PostJSON do
+  alias EpochtalkServerWeb.BoardJSON
 
   @moduledoc """
   Renders and formats `Post` data, in JSON format for frontend
@@ -22,16 +23,23 @@ defmodule EpochtalkServerWeb.PostJSON do
         limit: limit,
         desc: desc
       }) do
+    # format board data
+    board =
+      BoardJSON.format_board_data_for_find(
+        board_moderators,
+        board_mapping,
+        thread.board_id,
+        user_priority
+      )
+
     %{
+      board: board,
       posts: posts,
       poll: poll,
       thread: thread,
       write_access: write_access,
       board_banned: board_banned,
       user: user,
-      user_priority: user_priority,
-      board_mapping: board_mapping,
-      board_moderators: board_moderators,
       page: page,
       start: start,
       limit: limit,

6033bdcfd536e23fbbd55fa7c422882d0e07c940
unenglishable <unenglishable@gmail.com>
2023-06-08 11:48:40 -0700

    test(seeds/thread): seed more threads

--- test/seed/threads.exs
+++ test/seed/threads.exs
@@ -1,17 +1,41 @@
 alias EpochtalkServer.Models.Thread
 alias EpochtalkServer.Repo
 
-thread = %{
-  "title" => "test",
-  "body" => "testing thread",
-  "board_id" => 1,
-  "sticky" => false,
-  "locked" => false,
-  "moderated" => false,
-  "addPoll" => false,
-  "pollValid" => false,
-  "slug" => "test_slug"
-}
+threads = [
+  %{
+    "title" => "test",
+    "body" => "testing thread",
+    "board_id" => 1,
+    "sticky" => false,
+    "locked" => false,
+    "moderated" => false,
+    "addPoll" => false,
+    "pollValid" => false,
+    "slug" => "test_slug"
+  },
+  %{
+    "title" => "test2",
+    "body" => "testing thread2",
+    "board_id" => 1,
+    "sticky" => false,
+    "locked" => false,
+    "moderated" => false,
+    "addPoll" => false,
+    "pollValid" => false,
+    "slug" => "test_slug2"
+  },
+  %{
+    "title" => "test3",
+    "body" => "testing thread3",
+    "board_id" => 1,
+    "sticky" => false,
+    "locked" => false,
+    "moderated" => false,
+    "addPoll" => false,
+    "pollValid" => false,
+    "slug" => "test_slug3"
+  },
+]
 
 admin_board_thread = %{
   "title" => "admin board thread test",
@@ -38,15 +62,18 @@ super_admin_board_thread = %{
 }
 
 Repo.transaction(fn ->
-  Thread.create(thread, 1)
-  |> case do
-    {:ok, _} ->
-      IO.puts("Successfully seeded test thread")
+  threads
+  |> Enum.each(fn thread ->
+    Thread.create(thread, 1)
+    |> case do
+      {:ok, _} ->
+        IO.puts("Successfully seeded test thread")
 
-    {:error, error} ->
-      IO.puts("Error seeding test thread")
-      IO.inspect(error)
-  end
+      {:error, error} ->
+        IO.puts("Error seeding test thread")
+        IO.inspect(error)
+    end
+  end)
 end)
 
 Repo.transaction(fn ->

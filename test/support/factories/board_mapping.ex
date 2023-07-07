65e0e63e7405a7b4bcc162cd4e8187e49ab71b44
unenglishable <unenglishable@gmail.com>
2023-07-07 14:42:56 -0700

    test(factories/board_mapping): add board mapping factory

---
+++ test/support/factories/board_mapping.ex
@@ -0,0 +1,37 @@
+defmodule Test.Support.Factories.BoardMapping do
+  @moduledoc """
+  Factory for `BoardMapping`
+  """
+  alias EpochtalkServer.Models.BoardMapping
+
+  defmacro __using__(_opts) do
+    quote do
+      def board_mapping_factory(%{board: board, view_order: view_order, category: category}) do
+        %{
+          id: board.id,
+          name: board.name,
+          type: "board",
+          category_id: category.id,
+          view_order: view_order
+        }
+      end
+      def board_mapping_factory(%{board: board, view_order: view_order, parent: parent}) do
+        %{
+          id: board.id,
+          name: board.name,
+          type: "board",
+          board_id: parent.id,
+          view_order: view_order
+        }
+      end
+      def board_mapping_factory(%{category: category, view_order: view_order}) do
+        %{
+          id: category.id,
+          name: category.name,
+          type: "category",
+          view_order: view_order
+        }
+      end
+    end
+  end
+end

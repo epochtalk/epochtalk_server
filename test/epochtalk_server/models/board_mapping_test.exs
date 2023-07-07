4bf0ea8c6edd80d9f3a7cef3c3b3fb7cd0088915
unenglishable <unenglishable@gmail.com>
2023-07-07 14:59:32 -0700

    test(models/board_mapping): use board mapping factory

--- test/epochtalk_server/models/board_mapping_test.exs
+++ test/epochtalk_server/models/board_mapping_test.exs
@@ -13,44 +13,13 @@ defmodule Test.EpochtalkServer.Models.BoardMapping do
       category_board2 = insert(:board)
       child_board1 = insert(:board)
       child_board2 = insert(:board)
-
-      board_mapping_attrs = [
-        %{
-          id: category.id,
-          name: category.name,
-          type: "category",
-          view_order: 0
-        },
-        %{
-          id: category_board1.id,
-          name: category_board1.name,
-          type: "board",
-          category_id: category.id,
-          view_order: 1
-        },
-        %{
-          id: category_board2.id,
-          name: category_board2.name,
-          type: "board",
-          category_id: category.id,
-          view_order: 2
-        },
-        %{
-          id: child_board1.id,
-          name: child_board1.name,
-          type: "board",
-          board_id: category_board1.id,
-          view_order: 3
-        },
-        %{
-          id: child_board2.id,
-          name: child_board2.name,
-          type: "board",
-          board_id: category_board1.id,
-          view_order: 4
-        }
-      ]
-      BoardMapping.update(board_mapping_attrs)
+      BoardMapping.update([
+        build(:board_mapping, category: category, view_order: 0),
+        build(:board_mapping, board: category_board1, category: category, view_order: 1),
+        build(:board_mapping, board: category_board2, category: category, view_order: 2),
+        build(:board_mapping, board: child_board1, parent: category_board1, view_order: 3),
+        build(:board_mapping, board: child_board2, parent: category_board1, view_order: 4)
+      ])
 
       assert BoardMapping.all() |> Enum.count() == 4
     end
@@ -58,27 +27,14 @@ defmodule Test.EpochtalkServer.Models.BoardMapping do
 
   describe "update/1" do
     test "updates a board mapping" do
-      category = insert(:category)
-      board = insert(:board)
       initial_board_mappings_count = BoardMapping.all() |> Enum.count()
 
-      board_mapping_attrs = [
-        %{
-          id: category.id,
-          name: category.name,
-          type: "category",
-          view_order: 0
-        },
-        %{
-          id: board.id,
-          name: board.name,
-          type: "board",
-          category_id: category.id,
-          view_order: 1
-        }
-      ]
-
-      result = BoardMapping.update(board_mapping_attrs)
+      category = insert(:category)
+      board = insert(:board)
+      result = BoardMapping.update([
+        build(:board_mapping, category: category, view_order: 0),
+        build(:board_mapping, board: board, category: category, view_order: 1),
+      ])
       assert result == {:ok, :ok}
 
       assert BoardMapping.all() |> Enum.count() == 1
@@ -88,24 +44,13 @@ defmodule Test.EpochtalkServer.Models.BoardMapping do
   describe "delete_board_by_id/1" do
     test "deletes a board mapping" do
       assert BoardMapping.all() |> Enum.count() == 0
+
       category = insert(:category)
       board = insert(:board)
-      board_mapping_attrs = [
-        %{
-          id: category.id,
-          name: category.name,
-          type: "category",
-          view_order: 0
-        },
-        %{
-          id: board.id,
-          name: board.name,
-          type: "board",
-          category_id: category.id,
-          view_order: 1
-        }
-      ]
-      BoardMapping.update(board_mapping_attrs)
+      BoardMapping.update([
+        build(:board_mapping, category: category, view_order: 0),
+        build(:board_mapping, board: board, category: category, view_order: 1),
+      ])
       assert BoardMapping.all() |> Enum.count() == 1
 
       BoardMapping.delete_board_by_id(board.id)

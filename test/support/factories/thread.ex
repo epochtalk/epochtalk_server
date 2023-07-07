54587a1156d1a1d087ca4051f6a7aa6b8d763b34
unenglishable <unenglishable@gmail.com>
2023-07-07 16:28:04 -0700

    test(factories/thread): fix attribute factory, use "" =>

--- test/support/factories/thread.ex
+++ test/support/factories/thread.ex
@@ -8,15 +8,15 @@ defmodule Test.Support.Factories.Thread do
     quote do
       def thread_attributes_factory(%{board: board} = attrs) do
         %{
-          title: sequence(:thread_title, &"Thread title #{&1}"),
-          body: sequence(:thread_body, &"Thread body #{&1}"),
-          board_id: board.id,
-          sticky: Map.get(attrs, :sticky) || false,
-          locked: Map.get(attrs, :locked) || false,
-          moderated: false,
-          addPoll: false,
-          pollValid: false,
-          slug: sequence(:thread_slug, &"thread-slug-#{&1}")
+          "title" => sequence(:thread_title, &"Thread title #{&1}"),
+          "body" => sequence(:thread_body, &"Thread body #{&1}"),
+          "board_id" => board.id,
+          "sticky" => Map.get(attrs, :sticky) || false,
+          "locked" => Map.get(attrs, :locked) || false,
+          "moderated" => false,
+          "addPoll" => false,
+          "pollValid" => false,
+          "slug" => sequence(:thread_slug, &"thread-slug-#{&1}")
         }
       end
     end

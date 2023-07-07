164712f26e4c70eb2b547ca543de55bd1b1118fa
unenglishable <unenglishable@gmail.com>
2023-07-07 16:10:10 -0700

    test(factories/user): implement user factory

---
+++ test/support/factories/user.ex
@@ -0,0 +1,32 @@
+defmodule Test.Support.Factories.User do
+  @moduledoc """
+  Factory for `User`
+  """
+  alias EpochtalkServer.Models.User
+
+  defmacro __using__(_opts) do
+    quote do
+      def user_attributes_factory do
+        %{
+          username: sequence(:user_username, &"username#{&1}"),
+          email: sequence(:user_email, &"email#{&1}@test.com"),
+          password: sequence(:user_password, &"password#{&1}")
+        }
+      end
+      def user_factory(%{admin: true}) do
+        build(:user_attributes)
+        |> User.create(true)
+        |> case do
+          {:ok, user} -> user
+        end
+      end
+      def user_factory(_attrs) do
+        build(:user_attributes)
+        |> User.create()
+        |> case do
+          {:ok, user} -> user
+        end
+      end
+    end
+  end
+end

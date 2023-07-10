0e066964acfeb4d5fce5be29075df56fca468bf4
unenglishable <unenglishable@gmail.com>
2023-07-10 14:35:01 -0700

    test(controllers/board): check parent board in by_category

--- test/epochtalk_server_web/controllers/board_test.exs
+++ test/epochtalk_server_web/controllers/board_test.exs
@@ -29,7 +29,7 @@ defmodule Test.EpochtalkServerWeb.Controllers.Board do
   end
 
   describe "by_category/2" do
-    test "finds all active boards", %{conn: conn, category: category, parent_board: board} do
+    test "finds all active boards", %{conn: conn, category: category, parent_board: parent_board, child_board: child_board} do
       response =
         conn
         |> get(Routes.board_path(conn, :by_category))
@@ -50,39 +50,29 @@ defmodule Test.EpochtalkServerWeb.Controllers.Board do
         "id" => response_category_id,
         "name" => response_category_name,
         "view_order" => response_category_view_order,
-        "boards" => [
-          %{
-            "id" => response_parent_board_id,
-            "board_id" => response_parent_board_board_id,
-            "name" => response_parent_board_name,
-            "category_id" => response_parent_board_category_id,
-            "children" => response_parent_board_children,
-            "description" => response_parent_board_description,
-            "view_order" => response_parent_board_view_order,
-            "slug" => response_parent_board_slug,
-            "viewable_by" => response_parent_board_viewable_by,
-            "postable_by" => response_parent_board_postable_by,
-            "right_to_left" => response_parent_board_right_to_left
-          }
-        ]
+        "boards" => response_boards
       } = response |> Enum.at(0)
 
       # test category/board info
       assert response_category_id == category.id
       assert response_category_name == category.name
       assert response_category_view_order == 0
-      assert response_parent_board_id == board.id
-      assert response_parent_board_board_id == board.id
-      assert response_parent_board_name == board.name
-      assert response_parent_board_category_id == category.id
-      assert response_parent_board_children |> Enum.count() == 0
-      assert response_parent_board_description == board.description
-      assert response_parent_board_name == board.name
-      assert response_parent_board_view_order == 1
-      assert response_parent_board_slug == board.slug
-      assert response_parent_board_viewable_by == board.viewable_by
-      assert response_parent_board_postable_by == board.postable_by
-      assert response_parent_board_right_to_left == board.right_to_left
+
+      # extract parent board
+      [response_parent_board | response_boards] = response_boards
+      assert response_boards == []
+
+      # check parent board
+      assert response_parent_board["id"] == parent_board.id
+      assert response_parent_board["name"] == parent_board.name
+      assert response_parent_board["category_id"] == category.id
+      assert response_parent_board["children"] |> Enum.count == 1
+      assert response_parent_board["description"] == parent_board.description
+      assert response_parent_board["view_order"] == 1
+      assert response_parent_board["slug"] == parent_board.slug
+      assert response_parent_board["viewable_by"] == parent_board.viewable_by
+      assert response_parent_board["postable_by"] == parent_board.postable_by
+      assert response_parent_board["right_to_left"] == parent_board.right_to_left
     end
   end
 

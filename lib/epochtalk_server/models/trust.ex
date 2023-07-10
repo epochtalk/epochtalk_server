228a30092ff4bceb7b7091cd55d6f1f2a433ac02
Anthony Kinsey <anthony@slickage.com>
2023-07-10 13:53:11 -1000

    refactor(models/trust): wip implement function to calculate trust sources by user id

--- lib/epochtalk_server/models/trust.ex
+++ lib/epochtalk_server/models/trust.ex
@@ -50,4 +50,43 @@ defmodule EpochtalkServer.Models.Trust do
 
     Repo.all(query)
   end
+
+  ## === Public Helper Functions ===
+
+  def trust_sources_by_user_id(user_id, max_depth, debug) do
+    depth = 0..max_depth - 1 |> Enum.to_list()
+    %{sources: sources, untrusted: untrusted, last: last, debug: debug} =
+      Enum.reduce(depth, %{sources: [], untrusted: %{}, last: [user_id], debug: debug}, fn i, acc ->
+        debug = if acc.debug, do: Map.put(acc.debug, i, [])
+
+        if length(last) == 0 do
+          # do nothing
+          acc
+        else
+          trust_info = trust_by_user_ids(last)
+
+          votes = Enum.reduce(length(trust_info), %{}, fn x, acc ->
+            cur = Enum.at(trust_info, x)
+            cur_id = cur.user_id_trusted
+            acc = if is_nil(acc[cur_id]), do: Map.put(acc, cur_id, 0), else: acc
+            if cur.type == 1,
+              do: Map.put(acc, cur_id, acc[cur_id] - 1),
+              else: Map.put(acc, cur_id, acc[cur_id] + 1)
+          end)
+
+          %{sources: sources, untrusted: untrusted, last: last, debug: debug} =
+            Enum.reduce(length(trust_info), %{sources: acc.sources, untrusted: acc.untrusted, last: acc.last, debug: debug}, fn y, acc ->
+              cur_id = Enum.at(trust_info, y).user_id_trusted
+              cur_votes = votes[cur_id]
+            end)
+
+          %{
+            sources: sources,
+            untrusted: untrusted,
+            last: last,
+            debug: debug
+          }
+        end
+      end)
+  end
 end

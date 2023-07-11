29a54a195a3d899c57834b8afb367a832e65a703
Anthony Kinsey <anthony@slickage.com>
2023-07-11 12:30:55 -1000

    refactor(models/trust): refactor trust_sources_by_user_ids to fix credo errors

--- lib/epochtalk_server/models/trust.ex
+++ lib/epochtalk_server/models/trust.ex
@@ -54,14 +54,13 @@ defmodule EpochtalkServer.Models.Trust do
 
   ## === Public Helper Functions ===
 
-
   @doc """
-  Calculates the set of users in a user's `Trust` network, `max_depth` is codnfigured by the user
+  Calculates the set of users in a user's `Trust` network, `max_depth` is configured by the user
   """
   @spec trust_sources_by_user_id(user_id :: non_neg_integer, max_depth :: non_neg_integer, debug :: []) :: [non_neg_integer]
   def trust_sources_by_user_id(user_id, max_depth, debug) do
     depth = 0..max_depth - 1 |> Enum.to_list()
-    %{sources: sources, untrusted: _untrusted, last: _last, debug: debug} =
+    %{sources: sources, debug: debug} =
       Enum.reduce(depth, %{sources: [], untrusted: %{}, last: [user_id], debug: debug}, fn i, acc ->
         debug = if acc.debug, do: Map.put(acc.debug, i, [])
 
@@ -73,64 +72,20 @@ defmodule EpochtalkServer.Models.Trust do
 
           trust_info_len_enum = 0..length(trust_info) - 1 |> Enum.to_list()
 
-          votes = Enum.reduce(trust_info_len_enum, %{}, fn x, acc ->
-            cur = Enum.at(trust_info, x)
-            cur_id = cur.user_id_trusted
-            acc = if is_nil(acc[cur_id]), do: Map.put(acc, cur_id, 0), else: acc
-            if cur.type == 1,
-              do: Map.put(acc, cur_id, acc[cur_id] - 1),
-              else: Map.put(acc, cur_id, acc[cur_id] + 1)
-          end)
+          votes = update_votes(trust_info, trust_info_len_enum)
 
           %{sources: sources, untrusted: untrusted, last: last, debug: debug} =
-            Enum.reduce(trust_info_len_enum, %{sources: acc.sources, untrusted: acc.untrusted, last: [], debug: debug}, fn y, acc ->
-              cur_id = Enum.at(trust_info, y).user_id_trusted
-              cur_votes = votes[cur_id]
-
-              debug = if acc.debug and !acc.untrusted[cur_id] do
-                debug_i_len_enum = 0..length(acc.debug[i]) - 1 |> Enum.to_list()
-                exists = Enum.filter(debug_i_len_enum, fn n -> Enum.at(acc.debug[i], n) == cur_id end) |> length > 0
-                updated_debug_i = if exists, do: acc.debug[i], else: acc.debug[i] ++ [[cur_id, cur_votes]]
-                Map.put(acc.debug, i, updated_debug_i)
-              end
-
-              trusted_source = votes[cur_id] >= 0 and !acc.untrusted[cur_id]
-
-              sources = if trusted_source,
-                do: acc.sources ++ [cur_id],
-                else: acc.sources
-
-              last = if trusted_source,
-                do: acc.last ++ [cur_id],
-                else: acc.last
-
-              untrusted = if trusted_source,
-                do: acc.untrusted,
-                else: Map.put(acc.untrusted, cur_id, 1)
-
-              %{
-                sources: sources,
-                untrusted: untrusted,
-                last: last,
-                debug: debug
-              }
-            end)
+            update_sources_untrusted_last_and_debug(acc, votes, trust_info, i, trust_info_len_enum, debug)
 
           no_trusted_sources = last == [] and i == 0 and Map.keys(votes) == []
 
           default_trust_user_id = if no_trusted_sources, do: User.get_default_trust_user_id()
 
-          sources = if no_trusted_sources,
-            do: sources ++ [default_trust_user_id],
-            else: sources
+          sources = if no_trusted_sources, do: sources ++ [default_trust_user_id], else: sources
 
-          last = if no_trusted_sources,
-            do: last ++ [default_trust_user_id],
-            else: last
+          last = if no_trusted_sources, do: last ++ [default_trust_user_id], else: last
 
-          debug = if no_trusted_sources and debug do
-            Map.put(debug, 0, Enum.at(debug, 0) ++ [[default_trust_user_id, 0]])
-          end
+          debug = update_debug(debug, default_trust_user_id, no_trusted_sources)
 
           %{
             sources: sources,
@@ -143,8 +98,62 @@ defmodule EpochtalkServer.Models.Trust do
 
     sources = Enum.uniq(sources ++ [user_id])
 
-    debug = if debug, do: Map.put(debug, 0, Enum.at(debug, 0) ++ [[user_id, 0]]) |> Logger.debug()
+    if debug, do: Map.put(debug, 0, Enum.at(debug, 0) ++ [[user_id, 0]]) |> Logger.debug()
 
     sources
+  end
+
+  ## === Private Helper Functions ===
+
+  defp update_votes(trust_info, len_enum) do
+    Enum.reduce(len_enum, %{}, fn x, acc ->
+      cur = Enum.at(trust_info, x)
+      cur_id = cur.user_id_trusted
+      acc = if is_nil(acc[cur_id]), do: Map.put(acc, cur_id, 0), else: acc
+      if cur.type == 1,
+        do: Map.put(acc, cur_id, acc[cur_id] - 1),
+        else: Map.put(acc, cur_id, acc[cur_id] + 1)
+    end)
+  end
+
+  defp update_sources_untrusted_last_and_debug(acc, votes, trust_info, i, len_enum, debug) do
+    Enum.reduce(len_enum, %{sources: acc.sources, untrusted: acc.untrusted, last: [], debug: debug}, fn y, acc ->
+      cur_id = Enum.at(trust_info, y).user_id_trusted
+      cur_votes = votes[cur_id]
+
+      debug = if acc.debug and !acc.untrusted[cur_id] do
+        debug_i_len_enum = 0..length(acc.debug[i]) - 1 |> Enum.to_list()
+        exists = Enum.filter(debug_i_len_enum, fn n -> Enum.at(acc.debug[i], n) == cur_id end) |> length > 0
+        updated_debug_i = if exists, do: acc.debug[i], else: acc.debug[i] ++ [[cur_id, cur_votes]]
+        Map.put(acc.debug, i, updated_debug_i)
+      end
+
+      trusted_source = votes[cur_id] >= 0 and !acc.untrusted[cur_id]
+
+      sources = if trusted_source,
+        do: acc.sources ++ [cur_id],
+        else: acc.sources
+
+      last = if trusted_source,
+        do: acc.last ++ [cur_id],
+        else: acc.last
+
+      untrusted = if trusted_source,
+        do: acc.untrusted,
+        else: Map.put(acc.untrusted, cur_id, 1)
+
+      %{
+        sources: sources,
+        untrusted: untrusted,
+        last: last,
+        debug: debug
+      }
+    end)
+  end
+
+  defp update_debug(debug, default_trust_user_id, no_trusted_sources) do
+    if no_trusted_sources and debug do
+      Map.put(debug, 0, Enum.at(debug, 0) ++ [[default_trust_user_id, 0]])
+    end
   end
 end

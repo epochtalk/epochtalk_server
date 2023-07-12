f6601d3ee6ef7890fda15e10786dbeddcbfb110f
unenglishable <unenglishable@gmail.com>
2023-07-12 13:15:54 -0700

    test(seed/users): seed users with user factory

--- test/seed/users.exs
+++ test/seed/users.exs
@@ -1,37 +1,31 @@
-alias EpochtalkServer.Models.User
+import Test.Support.Factory
+
+test_super_admin_user_username = "superadmin"
+test_super_admin_user_email = "superadmin@test.com"
+test_super_admin_user_password = "password"
+
+test_admin_user_username = "admin"
+test_admin_user_email = "admin@test.com"
+test_admin_user_password = "password"
 
 test_user_username = "test"
 test_user_email = "test@test.com"
 test_user_password = "password"
 
-test_admin_user_username = "admin"
-test_admin_user_email = "admin@test.com"
-test_admin_user_password = "password"
-test_admin_user_admin = true
-
-User.create(%{username: test_user_username, email: test_user_email, password: test_user_password})
-|> case do
-  {:ok, _} ->
-    IO.puts("Successfully seeded test user")
-
-  {:error, error} ->
-    IO.puts("Error seeding test user")
-    IO.inspect(error)
-end
-
-User.create(
-  %{
-    username: test_admin_user_username,
-    email: test_admin_user_email,
-    password: test_admin_user_password
-  },
-  test_admin_user_admin
+build(:user_with_attributes,
+  username: test_super_admin_user_username,
+  email: test_super_admin_user_email,
+  password: test_super_admin_user_password
+) |> with_role_id(1)
+build(:user_with_attributes,
+  username: test_admin_user_username,
+  email: test_admin_user_email,
+  password: test_admin_user_password
+) |> with_role_id(2)
+build(:user_with_attributes,
+  username: test_user_username,
+  email: test_user_email,
+  password: test_user_password
 )
-|> case do
-  {:ok, _} ->
-    IO.puts("Successfully seeded test admin user")
 
-  {:error, error} ->
-    IO.puts("Error seeding test admin user")
-    IO.inspect(error)
-end
+IO.puts("Successfully seeded test users")

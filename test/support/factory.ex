850040c1cc24e2516e835430ee09bf33c068ded0
unenglishable <unenglishable@gmail.com>
2023-07-13 12:42:38 -0700

    test(factories): implement banned address

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -12,4 +12,5 @@ defmodule Test.Support.Factory do
   use Test.Support.Factories.Category
   use Test.Support.Factories.Board
   use Test.Support.Factories.Thread
+  use Test.Support.Factories.BannedAddress
 end

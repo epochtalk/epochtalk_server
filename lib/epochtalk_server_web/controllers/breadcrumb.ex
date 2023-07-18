8e0c53672ae05b814d6f6ca9dd20ba382dc280a4
Chris Rodrigues <crod951@gmail.com>
2023-07-18 00:46:03 -0700

    feat(breadcrumbs): Added '/breadcrumbs' route, added helper to create breadcrumbs recursively

---
+++ lib/epochtalk_server_web/controllers/breadcrumb.ex
@@ -0,0 +1,33 @@
+defmodule EpochtalkServerWeb.Controllers.Breadcrumb do
+  use EpochtalkServerWeb, :controller
+
+  @moduledoc """
+  Controller For `Breadcrumbs` related API requests
+  """
+  alias EpochtalkServerWeb.Helpers.Breadcrumbs
+  alias EpochtalkServerWeb.ErrorHelpers
+  alias EpochtalkServerWeb.Helpers.Validate
+
+  @doc """
+  Used to return breadcrumbs to the frontend`
+  """
+  def breadcrumbs(conn, attrs) do
+    with id <- parse_id(attrs, attrs["id"]),
+         type <- Validate.cast(attrs, "type", :string, required: true),
+         {:ok, breadcrumbs} <- Breadcrumbs.build_crumbs(id, type, []) do
+      render(conn, :breadcrumbs, breadcrumbs: breadcrumbs)
+    else
+      {:error, data} ->
+        ErrorHelpers.render_json_error(conn, 400, data)
+
+      _ ->
+        ErrorHelpers.render_json_error(conn, 500, "There was an issue obtaining the breadcrumbs")
+    end
+  end
+
+  defp parse_id(attrs, id) when is_integer(id),
+    do: Validate.cast(attrs, "id", :integer, min: 1, required: true)
+
+  defp parse_id(attrs, id) when is_binary(id),
+    do: Validate.cast(attrs, "id", :string, required: true)
+end

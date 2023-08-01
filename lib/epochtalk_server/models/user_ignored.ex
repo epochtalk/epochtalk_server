fd6e11fe64a4b83375dc341f991698f495c5cb07
Anthony Kinsey <anthony@slickage.com>
2023-08-01 13:56:11 -1000

    feat(models/user_ignored): implement function which returns lists of users ignored users

--- lib/epochtalk_server/models/user_ignored.ex
+++ lib/epochtalk_server/models/user_ignored.ex
@@ -1,5 +1,7 @@
 defmodule EpochtalkServer.Models.UserIgnored do
   use Ecto.Schema
+  import Ecto.Query
+  alias EpochtalkServer.Repo
   alias EpochtalkServer.Models.User
   alias EpochtalkServer.Models.UserIgnored
 
@@ -23,5 +25,21 @@ defmodule EpochtalkServer.Models.UserIgnored do
     belongs_to :user, User
     belongs_to :ignored_user, User
     field :created_at, :naive_datetime
+  end
+
+  ## === Database Functions ===
+
+  @doc """
+  Used to get `UserIgnored` data for a specific `User` on a list of `user_id`
+  """
+  @spec by_user_ids(user_id :: non_neg_integer, user_ids :: [non_neg_integer]) ::
+          ignored_users_list :: [non_neg_integer]
+  def by_user_ids(user_id, user_ids) when is_integer(user_id) and is_list(user_ids) do
+    query =
+      from u in UserIgnored,
+        where: u.user_id == ^user_id and u.ignored_user_id in ^user_ids,
+        select: u.ignored_user_id
+
+    Repo.all(query)
   end
 end

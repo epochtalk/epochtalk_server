6a4318d8bf2eb030f478efaa9887429879ff61a2
Anthony Kinsey <anthony@slickage.com>
2023-09-07 15:03:26 -1000

    refactor(models/user_activity): wip implement user activity algorithm/hook

--- lib/epochtalk_server/models/user_activity.ex
+++ lib/epochtalk_server/models/user_activity.ex
@@ -5,6 +5,12 @@ defmodule EpochtalkServer.Models.UserActivity do
   alias EpochtalkServer.Models.User
   alias EpochtalkServer.Models.UserActivity
 
+  @period_days 14
+  @hours 24
+  @minutes 60
+  @seconds 60
+  @milliseconds 1000
+
   @moduledoc """
   `UserActivity` model, for performing actions relating to `UserActivity`
   """
@@ -48,6 +54,46 @@ defmodule EpochtalkServer.Models.UserActivity do
     Repo.one(query) || 0
   end
 
+  @doc """
+  Updates `UserActivity` using the following algorithm
+
+  1) Is current_period_start null, if so populate current_period_start and current_period_offset with user registration date
+  2) check if current date is past (current_period_start + 14) if so update current_period_start and remaining_period_activity set to 14 and set current_period_offset to match start
+  3) query remaining_period_activity if 0 then return else move to step 4
+
+  # algorithm
+  4) current_period_offset - (current_period_start + 14 days) postsFound = (Query posts between this time for user)
+  5) if (postsFound >= remaining_period_activity) {
+       total_activity += remaining_period_activity;
+       remaining_period_activity = 0;
+     }
+    else {
+      total_activity += postsFound;
+      remaining_period_activity = remaining_period_activity - postsFound;
+    }
+
+  7) update total_activity, remaining_period_activity current_period_offset
+  """
+  @spec update(user_id :: non_neg_integer) :: {non_neg_integer, nil}
+  def update(user_id) do
+    query =
+      from u in User,
+        left_join: b in UserActivity,
+        on: b.user_id == ^user_id,
+        where: u.id == ^user_id,
+        select: %{
+          registered_at: u.created_at,
+          user_id: u.id,
+          current_period_start: b.current_period_start,
+          current_period_offset: b.current_period_offset,
+          remaining_period_activity: b.remaining_period_activity,
+          total_activity: b.total_activity
+        }
+
+    db_info = Repo.one(query)
+
+  end
+
   ## === Public Helper Functions ===
 
   @doc """
@@ -61,5 +107,16 @@ defmodule EpochtalkServer.Models.UserActivity do
         do: post,
         else: post |> Map.put(:user_activity, UserActivity.get_by_user_id(post.user_id))
     end)
+  end
+
+  @doc """
+  Used to update `UserActivity` upon creating a `Post` or `Thread`. Removes newbie
+  `Role` from `User` if activity reaches 30 or greater. Sends websocket notification
+  to reauthenticate `User` after `Role` is removed.
+  """
+  @spec update_user_activity(user_id :: non_neg_integer) :: :ok
+  def update_user_activity(user_id) do
+    # TODO(akinsey): implement after update_user_activity
+    :ok
   end
 end

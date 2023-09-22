e93741ce76c01449254f0ae450014c10aa52167e
unenglishable <unenglishable@gmail.com>
2023-09-22 11:30:02 -0700

    test(models/profile): test upsert for avatar update

--- test/epochtalk_server/models/profile_test.exs
+++ test/epochtalk_server/models/profile_test.exs
@@ -1,7 +1,16 @@
 defmodule Test.EpochtalkServer.Models.Profile do
   use Test.Support.DataCase, async: true
   alias EpochtalkServer.Models.Profile
+  alias EpochtalkServer.Models.User
 
+  describe "upsert/2" do
+    test "given valid user_id, updates user's avatar", %{users: %{user: user}} do
+      assert user.profile.avatar == ""
+      Profile.upsert(user.id, %{avatar: "image.png"})
+      {:ok, updated_user} = User.by_id(user.id)
+      assert updated_user.profile.avatar == "image.png"
+    end
+  end
   describe "create/2" do
     test "given invalid user_id, errors with Ecto.ConstraintError" do
       invalid_user_id = 0

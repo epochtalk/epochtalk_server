7686fdcebfd4f0325bdd58f8720fedb5651e658f
unenglishable <unenglishable@gmail.com>
2023-09-22 11:41:07 -0700

    test(models/profile): check profile default fields

--- test/epochtalk_server/models/profile_test.exs
+++ test/epochtalk_server/models/profile_test.exs
@@ -5,7 +5,15 @@ defmodule Test.EpochtalkServer.Models.Profile do
 
   describe "upsert/2" do
     test "given valid user_id, updates user's avatar", %{users: %{user: user}} do
+      # check default fields
       assert user.profile.avatar == ""
+      assert user.profile.position == nil
+      assert user.profile.signature == nil
+      assert user.profile.post_count == 0
+      assert user.profile.fields == nil
+      assert user.profile.raw_signature == nil
+      assert user.profile.last_active == nil
+
       Profile.upsert(user.id, %{avatar: "image.png"})
       {:ok, updated_user} = User.by_id(user.id)
       assert updated_user.profile.avatar == "image.png"

7e84f5a314ce50de0d528a3ef474882d86bc79b8
unenglishable <unenglishable@gmail.com>
2023-10-17 16:00:04 -0700

    fix(regex): user_id; remove bol from pattern, use 0-9 instead of [:digit]

--- lib/epochtalk_server/regex.ex
+++ lib/epochtalk_server/regex.ex
@@ -5,7 +5,7 @@ defmodule EpochtalkServer.Regex do
   @patterns %{
     username_mention: ~r/\[code\].*\[\/code\](*SKIP)(*FAIL)|(?<=^|\s)@([a-zA-Z0-9\-_.]+)/i,
     username_mention_curly: ~r/\[code\].*\[\/code\](*SKIP)(*FAIL)|{@([a-zA-Z0-9\-_.]+)}/i,
-    user_id: ~r/{@^[[:digit:]]+}/
+    user_id: ~r/{@[0-9]+}/
   }
 
   @doc """

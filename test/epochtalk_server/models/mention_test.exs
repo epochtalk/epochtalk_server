26e4ceacc912d34386c30bfba11e0b6cfc187fca
unenglishable <unenglishable@gmail.com>
2023-10-19 11:35:12 -0700

    test(models/mention): username_to_user_id/2
    check invalid mentions do not get generated

--- test/epochtalk_server/models/mention_test.exs
+++ test/epochtalk_server/models/mention_test.exs
@@ -2,4 +2,34 @@ defmodule Test.EpochtalkServer.Models.Mention do
   use Test.Support.ConnCase, async: true
   import Test.Support.Factory
   alias EpochtalkServer.Models.Mention
+
+  setup %{users: %{user: user}} do
+    category = insert(:category)
+    board = insert(:board)
+    build(:board_mapping,
+      attributes: [
+        [category: category, view_order: 0],
+        [board: board, category: category, view_order: 1]
+      ]
+    )
+    thread = build(:thread, board: board, user: user)
+    {:ok, thread: thread.post.thread}
+  end
+
+  describe "username_to_user_id/2" do
+    test "given a body with invalid mentions, does not generate mentions", %{thread: thread, users: %{user: user}} do
+      attrs = %{
+        "thread" => thread.id,
+        "title" => "title",
+        "body" => """
+        @invalid @not_valid @no_user
+        this post should not @generate @ny @mentions
+        """
+      }
+      result = Mention.username_to_user_id(user, attrs)
+      assert result["body"] == attrs["body"]
+      assert result["body_original"] == attrs["body"]
+      assert result["mentioned_ids"] == []
+    end
+  end
 end

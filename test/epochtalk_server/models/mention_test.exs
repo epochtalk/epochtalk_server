e76818f9470c63d15423b58e8795c5ac21159bd5
unenglishable <unenglishable@gmail.com>
2023-10-19 12:07:30 -0700

    test(models/mention): test empty title

--- test/epochtalk_server/models/mention_test.exs
+++ test/epochtalk_server/models/mention_test.exs
@@ -40,6 +40,17 @@ defmodule Test.EpochtalkServer.Models.Mention do
       assert result["body_original"] == attrs["body"]
       assert result["mentioned_ids"] == []
     end
+    test "given an empty title, succeeds", %{thread: thread, users: %{user: user}} do
+      attrs = %{
+        "thread" => thread.id,
+        "title" => "",
+        "body" => "body"
+      }
+      result = Mention.username_to_user_id(user, attrs)
+      assert result["body"] == attrs["body"]
+      assert result["body_original"] == attrs["body"]
+      assert result["mentioned_ids"] == []
+    end
     test "given an empty body, succeeds", %{thread: thread, users: %{user: user}} do
       attrs = %{
         "thread" => thread.id,

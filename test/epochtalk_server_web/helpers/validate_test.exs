02ec3ed90a4ba6444b2e8b2c449d93b11751b84f
unenglishable <unenglishable@gmail.com>
2023-10-31 14:27:30 -0700

    test(helpers/validate): mix format

--- test/epochtalk_server_web/helpers/validate_test.exs
+++ test/epochtalk_server_web/helpers/validate_test.exs
@@ -6,47 +6,48 @@ defmodule Test.EpochtalkServerWeb.Helpers.Validate do
 
   describe "mutually_exclusive/2" do
     test "given cases, returns correct result" do
-      cases =
-        [
-          %{
-            attrs: %{"page" => 1},
-            keys: ["page", "start"],
-            result: :ok
-          },
-          %{
-            attrs: %{"start" => 1},
-            keys: ["page", "start"],
-            result: :ok
-          },
-          %{
-            attrs: %{"start" => 1},
-            keys: ["start"],
-            result: :ok
-          },
-          %{
-            attrs: %{"page" => 1, "start" => 1},
-            keys: ["page", "start"],
-            result: :error
-          },
-          %{
-            attrs: %{"page" => 1, "start" => 1, "stop" => 1},
-            keys: ["page", "start"],
-            result: :error
-          },
-          %{
-            attrs: %{"page" => 1, "stop" => 1},
-            keys: ["page", "start", "stop", "enter"],
-            result: :error
-          }
-        ]
+      cases = [
+        %{
+          attrs: %{"page" => 1},
+          keys: ["page", "start"],
+          result: :ok
+        },
+        %{
+          attrs: %{"start" => 1},
+          keys: ["page", "start"],
+          result: :ok
+        },
+        %{
+          attrs: %{"start" => 1},
+          keys: ["start"],
+          result: :ok
+        },
+        %{
+          attrs: %{"page" => 1, "start" => 1},
+          keys: ["page", "start"],
+          result: :error
+        },
+        %{
+          attrs: %{"page" => 1, "start" => 1, "stop" => 1},
+          keys: ["page", "start"],
+          result: :error
+        },
+        %{
+          attrs: %{"page" => 1, "stop" => 1},
+          keys: ["page", "start", "stop", "enter"],
+          result: :error
+        }
+      ]
 
-      cases |> Enum.each(fn
+      cases
+      |> Enum.each(fn
         %{attrs: attrs, keys: keys, result: :error} ->
           assert_raise InvalidPayload,
-          ~r/^The following payload parameters cannot be passed at the same time:/,
-          fn ->
-            Validate.mutually_exclusive!(attrs, keys)
-          end
+                       ~r/^The following payload parameters cannot be passed at the same time:/,
+                       fn ->
+                         Validate.mutually_exclusive!(attrs, keys)
+                       end
+
         %{attrs: attrs, keys: keys, result: :ok} ->
           assert Validate.mutually_exclusive!(attrs, keys) == :ok
       end)

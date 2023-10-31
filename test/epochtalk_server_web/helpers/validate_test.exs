6ed9ede4f30ec4c951daf00155a1305c219c5526
unenglishable <unenglishable@gmail.com>
2023-10-31 14:23:52 -0700

    test(helpers/validate): test mutually exclusive with several cases

--- test/epochtalk_server_web/helpers/validate_test.exs
+++ test/epochtalk_server_web/helpers/validate_test.exs
@@ -1,21 +1,55 @@
 defmodule Test.EpochtalkServerWeb.Helpers.Validate do
   use Test.Support.ConnCase, async: true
   alias EpochtalkServerWeb.Helpers.Validate
+  alias EpochtalkServerWeb.CustomErrors.InvalidPayload
   doctest Validate
 
-  test "mutually_exclusive/2" do
-  ## Example
-    iex> alias EpochtalkServerWeb.Helpers.Validate
-    iex> attrs = %{"page" => 1}
-    iex> Validate.mutually_exclusive!(attrs, ["page", "start"])
-    :ok
-    iex> attrs = %{"start" => 1}
-    iex> Validate.mutually_exclusive!(attrs, ["page", "start"])
-    :ok
-    iex> Validate.mutually_exclusive!(attrs, ["start"])
-    :ok
-    iex> attrs = %{"page" => 1, "start" => 1}
-    iex> Validate.mutually_exclusive!(attrs, ["page", "start"])
-    ** (EpochtalkServerWeb.CustomErrors.InvalidPayload) The following payload parameters cannot be passed at the same time: page, start
+  describe "mutually_exclusive/2" do
+    test "given cases, returns correct result" do
+      cases =
+        [
+          %{
+            attrs: %{"page" => 1},
+            keys: ["page", "start"],
+            result: :ok
+          },
+          %{
+            attrs: %{"start" => 1},
+            keys: ["page", "start"],
+            result: :ok
+          },
+          %{
+            attrs: %{"start" => 1},
+            keys: ["start"],
+            result: :ok
+          },
+          %{
+            attrs: %{"page" => 1, "start" => 1},
+            keys: ["page", "start"],
+            result: :error
+          },
+          %{
+            attrs: %{"page" => 1, "start" => 1, "stop" => 1},
+            keys: ["page", "start"],
+            result: :error
+          },
+          %{
+            attrs: %{"page" => 1, "stop" => 1},
+            keys: ["page", "start", "stop", "enter"],
+            result: :error
+          }
+        ]
+
+      cases |> Enum.each(fn
+        %{attrs: attrs, keys: keys, result: :error} ->
+          assert_raise InvalidPayload,
+          ~r/^The following payload parameters cannot be passed at the same time:/,
+          fn ->
+            Validate.mutually_exclusive!(attrs, keys)
+          end
+        %{attrs: attrs, keys: keys, result: :ok} ->
+          assert Validate.mutually_exclusive!(attrs, keys) == :ok
+      end)
+    end
   end
 end

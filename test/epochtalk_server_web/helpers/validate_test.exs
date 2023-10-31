da3ed39f5a8c7b28de6ec9bd92a9cdcd5911fce5
unenglishable <unenglishable@gmail.com>
2023-10-31 13:17:59 -0700

    test(helpers/validate): move docs to test (for testing later)

--- test/epochtalk_server_web/helpers/validate_test.exs
+++ test/epochtalk_server_web/helpers/validate_test.exs
@@ -2,4 +2,20 @@ defmodule Test.EpochtalkServerWeb.Helpers.Validate do
   use Test.Support.ConnCase, async: true
   alias EpochtalkServerWeb.Helpers.Validate
   doctest Validate
+
+  test "mutually_exclusive/2" do
+  ## Example
+    iex> alias EpochtalkServerWeb.Helpers.Validate
+    iex> attrs = %{"page" => 1}
+    iex> Validate.mutually_exclusive!(attrs, ["page", "start"])
+    :ok
+    iex> attrs = %{"start" => 1}
+    iex> Validate.mutually_exclusive!(attrs, ["page", "start"])
+    :ok
+    iex> Validate.mutually_exclusive!(attrs, ["start"])
+    :ok
+    iex> attrs = %{"page" => 1, "start" => 1}
+    iex> Validate.mutually_exclusive!(attrs, ["page", "start"])
+    ** (EpochtalkServerWeb.CustomErrors.InvalidPayload) The following payload parameters cannot be passed at the same time: page, start
+  end
 end

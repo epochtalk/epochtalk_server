53452f30c03f9adecaf491e9f173541c5ddf94f4
Anthony Kinsey <anthony@slickage.com>
2023-11-09 12:39:49 -1000

    feat(helpers/sanitize): function for sanitizing message subject

--- lib/epochtalk_server_web/helpers/sanitize.ex
+++ lib/epochtalk_server_web/helpers/sanitize.ex
@@ -9,4 +9,11 @@ defmodule EpochtalkServerWeb.Helpers.Sanitize do
   @spec strip_html_from_title(title :: String.t(), attrs :: map) :: map()
   def strip_html_from_title(title, attrs) when is_binary(title) and is_map(attrs),
     do: Map.put(attrs, "title", HtmlSanitizeEx.strip_tags(title))
+
+  @doc """
+  Used to strip html from `Message` subject
+  """
+  @spec strip_html_from_subject(subject :: String.t(), attrs :: map) :: map()
+  def strip_html_from_subject(subject, attrs) when is_binary(subject) and is_map(attrs),
+    do: Map.put(attrs, "subject", HtmlSanitizeEx.strip_tags(subject))
 end

cc30c243504350b1396b7d949cc2b542acbba6e2
unenglishable <unenglishable@gmail.com>
2024-02-05 16:45:20 -1000

    feat(s3): implement s3 module
    Handles server-side communication with AWS S3

---
+++ lib/epochtalk_server/s3.ex
@@ -0,0 +1,21 @@
+defmodule EpochtalkServer.S3 do
+  @moduledoc """
+  Handles server-side communication with AWS S3
+  """
+  require Logger
+
+  @doc """
+  """
+  @spec generate_presigned_post(params :: map) :: ExAws.S3.presigned_post_result()
+  def generate_presigned_post(%{filename: filename}) do
+    config = Application.get_env(:epochtalk_server, __MODULE__)
+    opts = [
+      expires_in: config[:expire_after_hours],
+      content_length_range: [config[:min_size_mb], config[:max_size_mb]],
+      virtual_host: config[:virtual_host]
+    ]
+
+    ExAws.Config.new(:s3, [])
+    |> ExAws.S3.presigned_post(config[:bucket], config[:path] <> filename, opts)
+  end
+end

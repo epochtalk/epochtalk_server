e86ab2f159fc10d06776f2012e4545387b11885a
Anthony Kinsey <anthony@slickage.com>
2024-03-12 15:29:23 -1000

    feat(post-drafts): full implement post drafts with testing

--- lib/epochtalk_server/models/poll_answer.ex
+++ lib/epochtalk_server/models/poll_answer.ex
@@ -1,7 +1,6 @@
 defmodule EpochtalkServer.Models.PollAnswer do
   use Ecto.Schema
   import Ecto.Changeset
-  # import Ecto.Query
   alias EpochtalkServer.Repo
   alias EpochtalkServer.Models.PollAnswer
   alias EpochtalkServer.Models.Poll
--- lib/epochtalk_server/models/post_draft.ex
+++ lib/epochtalk_server/models/post_draft.ex
@@ -1,6 +1,7 @@
 defmodule EpochtalkServer.Models.PostDraft do
   use Ecto.Schema
   import Ecto.Query
+  import Ecto.Changeset
   alias EpochtalkServer.Repo
   alias EpochtalkServer.Models.User
   alias EpochtalkServer.Models.PostDraft
@@ -27,6 +28,27 @@ defmodule EpochtalkServer.Models.PostDraft do
     field :updated_at, :naive_datetime
   end
 
+  ## === Changeset Functions ===
+
+  @doc """
+  Create a changeset for upserting a `PostDraft`
+  """
+  @spec upsert_changeset(draft :: PostDraft.t(), attrs :: map() | nil) :: Ecto.Changeset.t()
+  def upsert_changeset(draft, attrs \\ %{}) do
+    updated_at = NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
+
+    draft =
+      draft
+      |> Map.put(:updated_at, updated_at)
+
+    draft
+    |> cast(attrs, [:user_id, :draft, :updated_at])
+    |> validate_required([:user_id, :updated_at])
+    |> validate_length(:draft, min: 1, max: 64_000)
+    |> unique_constraint(:user_id, name: :user_drafts_user_id_index)
+    |> foreign_key_constraint(:user_id, name: :user_drafts_user_id_fkey)
+  end
+
   ## === Database Functions ===
 
   @doc """
@@ -44,16 +66,20 @@ defmodule EpochtalkServer.Models.PostDraft do
   @doc """
   Used to upsert a `PostDraft` for a specific `User`
   """
-  @spec upsert(user_id :: non_neg_integer, draft :: String.t()) ::
+  @spec upsert(user_id :: non_neg_integer, attrs :: map()) ::
           {:ok, t()} | {:error, Ecto.Changeset.t()}
-  def upsert(user_id, draft) when is_integer(user_id) and is_binary(draft) do
-    now = NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
+  def upsert(user_id, attrs) when is_integer(user_id) do
+    post_draft_cs = upsert_changeset(%PostDraft{user_id: user_id}, attrs)
 
     Repo.insert(
-      %PostDraft{user_id: user_id, draft: draft, updated_at: now},
-      on_conflict: [set: [draft: draft, updated_at: now]],
+      post_draft_cs,
+      on_conflict: [
+        set: [
+          draft: Map.get(post_draft_cs.changes, :draft),
+          updated_at: Map.get(post_draft_cs.data, :updated_at)
+        ]
+      ],
       conflict_target: [:user_id]
     )
   end
-
 end

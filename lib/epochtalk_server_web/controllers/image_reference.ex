771823f743fbd6d7a0691c0909ef511c68634233
unenglishable <unenglishable@gmail.com>
2024-03-19 09:52:00 -0700

    feat(controllers/image_reference): implement validate_max_length
    check that images list length does not exceed max images

--- lib/epochtalk_server_web/controllers/image_reference.ex
+++ lib/epochtalk_server_web/controllers/image_reference.ex
@@ -19,15 +19,24 @@ defmodule EpochtalkServerWeb.Controllers.ImageReference do
     with :ok <- ACL.allow!(conn, "images.upload.request"),
          # unwrap list from _json
          attrs_list <- attrs_list["_json"],
-         # ensure list does not exceed max size
-         true <- length(attrs_list) <= @max_images,
+         # ensure list does not exceed max length
+         :ok <- validate_max_length(attrs_list, @max_images),
          casted_attrs_list <- Enum.map(attrs_list, &cast_upload_attrs/1),
          create_result <- ImageReference.create(casted_attrs_list) do
       render(conn, :s3_request_upload, %{
         create_result: create_result
       })
     else
+      {:max_length_error, message} -> ErrorHelpers.render_json_error(conn, 400, message)
       _ -> ErrorHelpers.render_json_error(conn, 400, "Error, image request upload failed")
+    end
+  end
+  defp validate_max_length(attrs_list, max_length) do
+    attrs_length = length(attrs_list)
+    if attrs_length <= max_length do
+      :ok
+    else
+      {:max_length_error, "Requested images amount #{attrs_length} exceededs max of #{max_length}"}
     end
   end
   defp cast_upload_attrs(attrs) do

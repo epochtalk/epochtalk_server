170090713fc10b4419317bf33fee22ee4231b65d
unenglishable <unenglishable@gmail.com>
2024-04-04 21:27:51 -0700

    docs(models/image_reference): remove unnecessary variable set
    returning instead of creating variable

--- lib/epochtalk_server/models/image_reference.ex
+++ lib/epochtalk_server/models/image_reference.ex
@@ -106,46 +106,44 @@ defmodule EpochtalkServer.Models.ImageReference do
           {:ok, presigned_post_results :: map()}
           | {:error, value :: t(), others :: t()}
   def create(attrs_list) when is_list(attrs_list) do
-    # [changesets]
-    image_reference_changesets =
-      attrs_list
-      |> Stream.with_index()
-      |> Stream.map(fn {attrs, index} ->
-        {create_changeset(%ImageReference{}, attrs), index}
+    attrs_list
+    |> Stream.with_index()
+    |> Stream.map(fn {attrs, index} ->
+      {create_changeset(%ImageReference{}, attrs), index}
+    end)
+    |> Stream.map(fn {image_reference_changeset, index} ->
+      uuid = image_reference_changeset.changes.uuid
+      insert_key = "image_reference_#{uuid}"
+      presigned_post_key = "#{index}"
+
+      Multi.new()
+      |> Multi.insert(insert_key, image_reference_changeset)
+      |> Multi.run(presigned_post_key, fn _repo, insert_result ->
+        image_reference = insert_result[insert_key]
+        # set presigned post parameters
+        filename = image_reference.uuid <> "." <> image_reference.type
+
+        # generate presigned post
+        presigned_post_result = S3.generate_presigned_post(%{filename: filename})
+        {:ok, presigned_post_result}
       end)
-      |> Stream.map(fn {image_reference_changeset, index} ->
-        uuid = image_reference_changeset.changes.uuid
-        insert_key = "image_reference_#{uuid}"
-        presigned_post_key = "#{index}"
+    end)
+    # build multi combined transaction
+    |> Enum.reduce(Multi.new(), &Multi.append/2)
+    |> Repo.transaction()
+    |> case do
+      {:ok, results} ->
+        presigned_post_results =
+          results
+          # return only indexed items
+          |> Enum.filter(&key_is_integer?/1)
+          |> Map.new()
 
-        Multi.new()
-        |> Multi.insert(insert_key, image_reference_changeset)
-        |> Multi.run(presigned_post_key, fn _repo, insert_result ->
-          image_reference = insert_result[insert_key]
-          # set presigned post parameters
-          filename = image_reference.uuid <> "." <> image_reference.type
+        {:ok, presigned_post_results}
 
-          # generate presigned post
-          presigned_post_result = S3.generate_presigned_post(%{filename: filename})
-          {:ok, presigned_post_result}
-        end)
-      end)
-      # build multi combined transaction
-      |> Enum.reduce(Multi.new(), &Multi.append/2)
-      |> Repo.transaction()
-      |> case do
-        {:ok, results} ->
-          results =
-            results
-            # return only indexed items
-            |> Enum.filter(&key_is_integer?/1)
-            |> Map.new()
-
-          {:ok, results}
-
-        {:error, :image_references, value, others} ->
-          {:error, value, others}
-      end
+      {:error, :image_references, value, others} ->
+        {:error, value, others}
+    end
   end
 
   def create(attrs) do

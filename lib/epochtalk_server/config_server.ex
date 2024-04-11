53cd937ad4a07ef61f165a09dff67a532c086fa1
unenglishable <unenglishable@gmail.com>
2024-04-11 11:16:19 -0700

    feat(config_server): implement config genserver
    returns config by module name

---
+++ lib/epochtalk_server/config_server.ex
@@ -0,0 +1,60 @@
+defmodule EpochtalkServer.ConfigServer do
+  use GenServer
+
+  # TODO(boka): move to config
+  @one_day_in_ms 1000 * 60 * 60 * 24
+  @one_hour_in_ms 1000 * 60 * 60
+  @max_images_per_day 1000
+  @max_images_per_hour 100
+
+  @moduledoc """
+  Config genserver, stores configs in memory for quick lookup
+  """
+
+  ## === genserver functions ====
+
+  @impl true
+  def init(:ok), do: {:ok, load()}
+
+  @impl true
+  def handle_call({:get, module}, _from, all_configs) do
+    {:reply, all_configs[module], all_configs}
+  end
+
+  ## === api functions ====
+
+  @doc """
+  Start genserver and create a reference for supervision tree
+  """
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  @doc """
+  Returns config for specified module
+  """
+  @spec by_module(module :: String.t()) :: map() | nil
+  def by_module(module) do
+    GenServer.call(__MODULE__, {:get, module})
+  end
+
+  ## === private functions ====
+
+  # loads configs
+  defp load() do
+    %{
+      "Elixir.EpochtalkServer.RateLimiter": %{
+        s3_daily: [
+          &("s3_request_upload:user:#{&1.id}"),
+          @one_day_in_ms,
+          @max_images_per_day
+        ],
+        s3_hourly: [
+          &("s3_request_upload:user:#{&1.id}"),
+          @one_hour_in_ms,
+          @max_images_per_hour
+        ]
+      }
+    }
+  end
+end

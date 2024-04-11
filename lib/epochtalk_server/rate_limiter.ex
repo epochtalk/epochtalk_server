6a2a96b2f6d9bfca74dff8c18a705926b68de93f
unenglishable <unenglishable@gmail.com>
2024-04-11 11:33:10 -0700

    refactor(rate_limiter): use type to make rate limit call generic

--- lib/epochtalk_server/rate_limiter.ex
+++ lib/epochtalk_server/rate_limiter.ex
@@ -5,23 +5,14 @@ defmodule EpochtalkServer.RateLimiter do
     # get configs
     configs = EpochtalkServer.ConfigServer.by_module(__MODULE__)
 
-    case type do
-      :s3_daily ->
-        [key_fn, period, limit] = configs[:s3_daily]
-        key_fn.(user.id)
-        |> check_rate_inc(period, limit, count)
-        |> case do
-          {:allow, count} -> {:allow, count}
-          {:deny, count} -> {:s3_daily, count}
-        end
-      :s3_hourly ->
-        [key_fn, period, limit] = configs[:s3_hourly]
-        key_fn.(user.id)
-        |> check_rate_inc(period, limit, count)
-        |> case do
-          {:allow, count} -> {:allow, count}
-          {:deny, count} -> {:s3_hourly, count}
-        end
+    [key_fn, period, limit] = configs[type]
+    user.id
+    # build key with user id for rate limit check
+    |> key_fn.()
+    |> check_rate_inc(period, limit, count)
+    |> case do
+      {:allow, count} -> {:allow, count}
+      {:deny, count} -> {type, count}
     end
   end
 end

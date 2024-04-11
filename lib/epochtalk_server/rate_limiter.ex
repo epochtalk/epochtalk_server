84ac30c5e44b31f7d7dec1155cc7a8dcaabbd771
unenglishable <unenglishable@gmail.com>
2024-04-11 11:18:41 -0700

    feat(rate_limiter): use config server
    [broken] does not work yet
    
    get configs from config server
    use configs to check rate limit

--- lib/epochtalk_server/rate_limiter.ex
+++ lib/epochtalk_server/rate_limiter.ex
@@ -1,22 +1,23 @@
 defmodule EpochtalkServer.RateLimiter do
-  # TODO(boka): move to config
-  @one_day_in_ms 1000 * 60 * 60 * 24
-  @one_hour_in_ms 1000 * 60 * 60
-  @max_images_per_day 1000
-  @max_images_per_hour 100
-
   import Hammer, only: [check_rate_inc: 4]
 
   def check_rate_limited(type, user, count) do
+    # get configs
+    configs = EpochtalkServer.ConfigServer.by_module(__MODULE__)
+
     case type do
       :s3_daily ->
-        check_rate_inc("s3_request_upload:user:#{user.id}", @one_day_in_ms, @max_images_per_day, count)
+        [key_fn, period, limit] = configs[:s3_daily]
+        key_fn(user)
+        |> check_rate_inc(period, limit, count)
         |> case do
           {:allow, count} -> {:allow, count}
           {:deny, count} -> {:s3_daily, count}
         end
       :s3_hourly ->
-        check_rate_inc("s3_request_upload:user:#{user.id}", @one_hour_in_ms, @max_images_per_hour, count)
+        [key_fn, period, limit] = configs[:s3_hourly]
+        key_fn(user)
+        |> check_rate_inc(period, limit, count)
         |> case do
           {:allow, count} -> {:allow, count}
           {:deny, count} -> {:s3_hourly, count}

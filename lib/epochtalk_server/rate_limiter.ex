2d9185cf81d4268699d0602c701952f4c499325f
unenglishable <unenglishable@gmail.com>
2024-04-11 13:25:53 -0700

    feat(rate_limiter): implement rate limit reset

--- lib/epochtalk_server/rate_limiter.ex
+++ lib/epochtalk_server/rate_limiter.ex
@@ -1,5 +1,8 @@
 defmodule EpochtalkServer.RateLimiter do
-  import Hammer, only: [check_rate_inc: 4]
+  import Hammer, only: [
+    check_rate_inc: 4,
+    delete_buckets: 1
+  ]
 
   def check_rate_limited(type, user, count) do
     # get configs
@@ -14,5 +17,20 @@ defmodule EpochtalkServer.RateLimiter do
       {:allow, count} -> {:allow, count}
       {:deny, count} -> {type, count}
     end
+  end
+
+  @doc """
+  Resets rate limit of specified type for specified user
+  """
+  @spec reset_rate_limit(type :: atom, user :: EpochtalkServer.Models.User.t()) :: {:ok, num_reset :: non_neg_integer}
+  def reset_rate_limit(type, user) do
+    # get configs
+    configs = EpochtalkServer.ConfigServer.by_module(__MODULE__)
+
+    [key_fn, _, _] = configs[type]
+    user.id
+    # build key with user id for rate limit check
+    |> key_fn.()
+    |> delete_buckets()
   end
 end

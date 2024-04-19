48649fa176384219b862f584b8685ca7e1bb9d19
unenglishable <unenglishable@gmail.com>
2024-04-19 10:20:11 -0700

    test(controllers/image_reference): request uploads with valid image counts

--- test/epochtalk_server_web/controllers/image_reference_test.exs
+++ test/epochtalk_server_web/controllers/image_reference_test.exs
@@ -35,5 +35,77 @@ defmodule Test.EpochtalkServerWeb.Controllers.ImageReference do
       assert Map.keys(response) == ["presigned_posts"]
       assert Map.keys(response["presigned_posts"]) |> length() == 0
     end
+
+    @tag authenticated: :admin
+    test "given list with one image, succeeds", %{
+      conn: conn
+    } do
+      images = [
+        %{
+          "length" => 100,
+          "file_type" => "jpeg"
+        }
+      ]
+      response =
+        conn
+        |> post(Routes.image_reference_path(conn, :s3_request_upload), %{images: images})
+        |> json_response(200)
+      assert Map.keys(response) == ["presigned_posts"]
+      assert Map.keys(response["presigned_posts"]) |> length() == 1
+    end
+
+    @tag authenticated: :admin
+    test "given list with ten images, succeeds", %{
+      conn: conn
+    } do
+      images = [
+        %{
+          "length" => 100,
+          "file_type" => "jpeg"
+        },
+        %{
+          "length" => 200,
+          "file_type" => "png"
+        },
+        %{
+          "length" => 300,
+          "file_type" => "gif"
+        },
+        %{
+          "length" => 400,
+          "file_type" => "tiff"
+        },
+        %{
+          "length" => 500,
+          "file_type" => "vnd.microsoft.icon"
+        },
+        %{
+          "length" => 600,
+          "file_type" => "x-icon"
+        },
+        %{
+          "length" => 700,
+          "file_type" => "vnd.djvu"
+        },
+        %{
+          "length" => 800,
+          "file_type" => "svg+xml"
+        },
+        %{
+          "length" => 900,
+          "file_type" => "jpeg"
+        },
+        %{
+          "length" => 1000,
+          "file_type" => "jpeg"
+        }
+      ]
+      response =
+        conn
+        |> post(Routes.image_reference_path(conn, :s3_request_upload), %{images: images})
+        |> json_response(200)
+      assert Map.keys(response) == ["presigned_posts"]
+      assert Map.keys(response["presigned_posts"]) |> length() == 10
+    end
   end
 end

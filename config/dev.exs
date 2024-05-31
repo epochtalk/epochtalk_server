4ddc8f1765b32d630e58da491fec196ccbb3c666
unenglishable <unenglishable@gmail.com>
2024-05-30 18:21:26 -1000

    refactor(config): consolidate endpoint configs in runtime

--- config/dev.exs
+++ config/dev.exs
@@ -1,21 +1,5 @@
 import Config
 
-# For development, we disable any cache and enable
-# debugging and code reloading.
-#
-# The watchers configuration can be used to run external
-# watchers to your application. For example, we use it
-# with esbuild to bundle .js and .css sources.
-config :epochtalk_server, EpochtalkServerWeb.Endpoint,
-  # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {0, 0, 0, 0, 0, 0, 0, 0}, port: 4000],
-  check_origin: false,
-  code_reloader: true,
-  debug_errors: false,
-  secret_key_base: "9ORa6oGSN+xlXNedSn0gIKVc/6//naQqSiZsRJ8vNbcvHpPOTPMLgcn134WIH3Pd",
-  watchers: []
-
 # For Development `config/dev.exs` loads the "Local" adapter which allows preview
 # of sent emails at the url `/dev/mailbox`. To test SMTP in Development mode,
 # mailer configurations for adapter, credentials and other options can be
--- config/test.exs
+++ config/test.exs
@@ -1,12 +1,5 @@
 import Config
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
-config :epochtalk_server, EpochtalkServerWeb.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: 4002],
-  secret_key_base: "sFacLki12NmjBfy3rJHugf+w+o36b07r99JfjcDAliabs3JdmQ4GAJKlgPVvaaah",
-  server: false
-
 # In test we don't send emails.
 config :epochtalk_server, EpochtalkServer.Mailer, adapter: Swoosh.Adapters.Test
 

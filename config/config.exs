847e1a239dc371593d24fe4ddbca0b630eb1b0a5
unenglishable <unenglishable@gmail.com>
2024-06-06 14:57:35 -1000

    refactor(config): remove endpoint configs from config/config,prod

--- config/config.exs
+++ config/config.exs
@@ -9,18 +9,9 @@ import Config
 
 # Set ecto repos
 config :epochtalk_server, ecto_repos: [EpochtalkServer.Repo]
+
 # Set Guardian.DB to GuardianRedis
 config :guardian, Guardian.DB, repo: GuardianRedis.Repo
-
-# Configures the endpoint
-config :epochtalk_server, EpochtalkServerWeb.Endpoint,
-  url: [host: "localhost"],
-  render_errors: [
-    formats: [json: EpochtalkServerWeb.Controllers.ErrorJSON],
-    layout: false
-  ],
-  pubsub_server: EpochtalkServer.PubSub,
-  live_view: [signing_salt: "2Ay27BWv"]
 
 # Swoosh API client is needed for adapters other than SMTP.
 config :swoosh, :api_client, false

94526f7883ae5ee542cec73206bbfbad1fec2a47
unenglishable <unenglishable@gmail.com>
2024-08-30 11:06:09 -0700

    test(controllers/breadcrumb): refactor with ~p sigil for verified routes

--- test/epochtalk_server_web/controllers/breadcrumb_test.exs
+++ test/epochtalk_server_web/controllers/breadcrumb_test.exs
@@ -39,7 +39,7 @@ defmodule Test.EpochtalkServerWeb.Controllers.Breadcrumb do
       response =
         conn
         |> get(
-          Routes.breadcrumb_path(conn, :breadcrumbs, %{"id" => category.id, "type" => "category"})
+          ~p"/api/breadcrumbs?#{[id: category.id, type: "category"]}"
         )
         |> json_response(200)
 
@@ -231,7 +231,7 @@ defmodule Test.EpochtalkServerWeb.Controllers.Breadcrumb do
          %{conn: conn} do
       response =
         conn
-        |> get(Routes.breadcrumb_path(conn, :breadcrumbs, %{"id" => 99, "type" => "category"}))
+        |> get(~p"/api/breadcrumbs?#{[id: 99, type: "category"]}")
         |> json_response(200)
 
       assert Enum.empty?(response["breadcrumbs"]) == true
@@ -242,7 +242,7 @@ defmodule Test.EpochtalkServerWeb.Controllers.Breadcrumb do
       response =
         conn
         |> get(
-          Routes.breadcrumb_path(conn, :breadcrumbs, %{"id" => "error_board", "type" => "board"})
+          ~p"/api/breadcrumbs?#{[id: "error_board", type: "board"]}"
         )
         |> json_response(200)
 
@@ -254,7 +254,7 @@ defmodule Test.EpochtalkServerWeb.Controllers.Breadcrumb do
       response =
         conn
         |> get(
-          Routes.breadcrumb_path(conn, :breadcrumbs, %{"id" => "test_thread", "type" => "thread"})
+          ~p"/api/breadcrumbs?#{[id: "test_thread", type: "thread"]}"
         )
         |> json_response(200)
 

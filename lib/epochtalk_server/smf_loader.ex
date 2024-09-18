2240cb19225d2a894b20bab4c8e71b5492398530
unenglishable <unenglishable@gmail.com>
2024-09-18 10:44:31 -0700

    feat(smf_loader): add smf_loader
    loads data from smf query output tsv file

---
+++ lib/epochtalk_server/smf_loader.ex
@@ -0,0 +1,23 @@
+defmodule EpochtalkServer.SMFLoader do
+  # loads smf data from a tsv file
+  def load_from_tsv_file(path) do
+    with true <- if(File.exists?(path), do: true, else: "ファイルがない"),
+      {:ok, file} <- File.open(path),
+      headers <-
+        IO.read(file, :line)
+        |> String.split("\t")
+        |> Enum.map(&(&1 |> String.trim)),
+      file_stream <-
+        IO.stream(file, :line)
+        |> Stream.map(&(&1 |> String.split("\t")))
+        |> Stream.map(fn line ->
+          # map each line to headers
+          Enum.zip(headers, line)
+          |> Enum.into(%{})
+        end) do
+          file_stream
+    else
+      problem -> IO.puts("問題がある： #{inspect(problem)}")
+    end
+  end
+end

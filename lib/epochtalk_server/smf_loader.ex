f590b8ff8d655d919c21685c367b7230c04bfb5a
unenglishable <unenglishable@gmail.com>
2024-09-24 10:36:38 -0700

    feat(smf_loader): map boards from smf to epochtalk format

--- lib/epochtalk_server/smf_loader.ex
+++ lib/epochtalk_server/smf_loader.ex
@@ -27,4 +27,45 @@ defmodule EpochtalkServer.SMFLoader do
       problem -> IO.puts("問題がある： #{inspect(problem)}")
     end
   end
+  def map_boards_stream(boards_stream) do
+    now = NaiveDateTime.utc_now() |> NaiveDateTime.to_string()
+    {boards_stream, _slug_duplicate_index} =
+      boards_stream
+      |> Enum.map_reduce(%{}, fn smf_board, slugs ->
+        slug =
+          smf_board["name"]
+          |> String.replace(~r{ }, "-")
+          |> String.slice(0..99)
+        # handle duplicate slugs
+        slug_duplicate_index = Map.get(slugs, slug)
+        {slugs, slug} =
+          if slug_duplicate_index == nil do
+            # keep track of used slugs, return original slug
+            {Map.put(slugs, slug, 0), slug}
+          else
+            # replace last characters with index
+            new_slug = slug |> String.slice(0..(99 - (1 + Integer.floor_div(slug_duplicate_index, 10))))
+            new_slug = new_slug <> to_string(slug_duplicate_index)
+            # increment used slugs, return new slug
+            {Map.put(slugs, slug, slug_duplicate_index + 1), new_slug}
+          end
+        board =
+          %{}
+          |> Map.put(:id, smf_board["ID_BOARD"])
+          |> Map.put(:name, smf_board["name"])
+          |> Map.put(:description, smf_board["description"])
+          |> Map.put(:post_count, smf_board["numPosts"])
+          |> Map.put(:thread_count, smf_board["numTopics"])
+          |> Map.put(:viewable_by, "")
+          |> Map.put(:postable_by, "")
+          |> Map.put(:created_at, now)
+          |> Map.put(:imported_at, now)
+          |> Map.put(:updated_at, now)
+          |> Map.put(:meta, "\"{\"\"disable_self_mod\"\": false, \"\"disable_post_edit\"\": null, \"\"disable_signature\"\": false}\"")
+          |> Map.put(:right_to_left, "f")
+          |> Map.put(:slug, slug)
+        {board, slugs}
+      end)
+    boards_stream
+  end
 end

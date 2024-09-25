6de12cf0db3165bffee190e6b70a8fe80c138121
unenglishable <unenglishable@gmail.com>
2024-09-25 13:21:35 -0700

    fix(smf_loader): switch file import mode for boards from Stream to File read
    fixes issue with ç converting to other characters

--- lib/epochtalk_server/smf_loader.ex
+++ lib/epochtalk_server/smf_loader.ex
@@ -22,27 +22,28 @@ defmodule EpochtalkServer.SMFLoader do
   # loads smf data from a tsv file
   def load_from_tsv_file(path) do
     with true <- if(File.exists?(path), do: true, else: "ファイルがない"),
-      {:ok, file} <- File.open(path),
+      {:ok, file} <- File.read(path),
+      file_lines <- file |> String.trim() |> String.split("\n"),
+      [header_line | file_lines] <- file_lines,
       headers <-
-        IO.read(file, :line)
         # clean line
+        header_line
         |> String.trim()
         |> String.split("\t"),
-        # |> Enum.map(&(&1 |> String.trim)),
-      file_stream <-
-        IO.stream(file, :line)
-        |> Stream.map(&(
+      smf_boards <-
+        file_lines
+        |> Enum.map(&(
           &1
           # clean line
           |> String.trim()
           |> String.split("\t")
         ))
-        |> Stream.map(fn line ->
+        |> Enum.map(fn line ->
           # map each line to headers
           Enum.zip(headers, line)
           |> Enum.into(%{})
         end) do
-          file_stream
+          smf_boards
     else
       problem -> IO.puts("問題がある： #{inspect(problem)}")
     end

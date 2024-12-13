288e2150f4f54a32d5b96e83d0462673b65ec0e1
Anthony Kinsey <anthony@slickage.com>
2024-12-12 16:06:50 -1000

    feat(script): implement script to sequentially hit by thread route and log when there is an error

---
+++ parse_by_thread.exs
@@ -0,0 +1,28 @@
+Mix.install([{:httpoison, "~> 2.0"}, {:logger_file_backend, "~> 0.0.10"},])
+
+defmodule ParseByThread do
+  require Logger
+
+  Logger.add_backend {LoggerFileBackend, :error}
+  Logger.configure_backend {LoggerFileBackend, :error},
+    path: "./parse_by_thread_error.log",
+    level: :error
+
+  @start_id 5_485_059
+  @end_id 1
+
+  def run do
+    for id <- @start_id..@end_id//-1 do
+      case HTTPoison.get("http://localhost:4000/api/posts?thread_id=#{id}") do
+        {:ok, %HTTPoison.Response{status_code: 200, body: _body}} ->
+          Logger.info("Successfully parsed thread with id (#{id})")
+        {:ok, %HTTPoison.Response{status_code: status_code}} ->
+          Logger.error("Thread with id (#{id}) received response with status code #{status_code}")
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          Logger.error("Thread with id (#{id}) HTTP request failed: #{reason}")
+      end
+    end
+  end
+end
+
+ParseByThread.run()
